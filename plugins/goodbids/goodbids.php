a31a28ecb4120b08393905c2f0dd178c66d00be2
Brian DiChiara <brian.dichiara@viget.com>
2023-12-04 16:44:12 -0600

    GoodBids Custom Plugin Init

---
+++ plugins/goodbids/goodbids.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:       GoodBids
+ * Plugin URI:        https://www.goodbids.org
+ * Description:       Custom functionality for GoodBids.org
+ * Version:           1.0.0
+ * Author:            Viget
+ * Author URI:        https://www.goodbids.org
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       goodbids
+ * Domain Path:       /languages
+ *
+ * @wordpress-plugin
+ * @link              https://www.goodbids.org
+ * @since             1.0.0
+ * @package           GoodBids
+ */
+
