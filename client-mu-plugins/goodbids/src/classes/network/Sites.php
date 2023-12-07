6645ae7e84315576b03b6657ceddec8f34bbd527
Brian DiChiara <brian.dichiara@viget.com>
2023-12-07 09:50:00 -0600

    Made Nonprofit fields more dynamic, added filter hook and documentation

--- client-mu-plugins/goodbids/src/classes/network/Sites.php
+++ client-mu-plugins/goodbids/src/classes/network/Sites.php
@@ -15,27 +15,71 @@ namespace GoodBids\Network;
 class Sites {
 
 	/**
+	 * Nonprofit custom fields
+	 *
+	 * @since 1.0.0
+	 * @var array
+	 */
+	private array $np_fields = [];
+
+	/**
 	 * @since 1.0.0
 	 */
 	public function __construct() {
+		$this->init_np_fields();
 		$this->validate_new_site_fields();
 		$this->new_site_form_fields();
 	}
 
 	/**
-	 * Add GoodBids Nonprofit fields to the new site form
+	 * Initialize Nonprofit custom fields.
 	 *
 	 * @since 1.0.0
 	 *
 	 * @return void
 	 */
-	private function new_site_form_fields() : void {
-		add_action(
-			'network_site_new_form',
-			function() {
-				require GOODBIDS_PLUGIN_PATH . 'views/network/new-site-fields.php';
-			}
+	private function init_np_fields() : void {
+		$this->np_fields = apply_filters(
+			'goodbids_nonprofit_custom_fields',
+			[
+				'gbnp-legal-name' => [
+					'label'       => __( 'Nonprofit Legal Name', 'goodbids' ),
+					'type'        => 'text',
+					'default'     => '',
+					'placeholder' => '',
+					'required'    => true,
+				],
+				'gbnp-ein'        => [
+					'label'       => __( 'Nonprofit EIN', 'goodbids' ),
+					'type'        => 'text',
+					'default'     => '',
+					'placeholder' => 'XX-XXXXXXX',
+					'required'    => true,
+				],
+				'gbnp-website'    => [
+					'label'       => __( 'Nonprofit Website', 'goodbids' ),
+					'type'        => 'url',
+					'default'     => '',
+					'placeholder' => 'https://',
+					'required'    => true,
+				],
+			]
 		);
+	}
+
+	/**
+	 * Retrieve array of Nonprofit custom fields.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array
+	 */
+	public function get_np_fields() : array {
+		if ( empty( $this->np_fields ) ) {
+			$this->init_np_fields();
+		}
+
+		return $this->np_fields;
 	}
 
 	/**
@@ -45,7 +89,7 @@ class Sites {
 	 *
 	 * @return void
 	 */
-	private function validate_new_site_fields() {
+	private function validate_new_site_fields() : void {
 		add_action(
 			'admin_init',
 			function() {
@@ -61,23 +105,38 @@ class Sites {
 
 				$blog = $_POST['blog']; // phpcs:ignore
 
-				$required = [
-					'gbnp-legal-name' => __( 'Nonprofit Legal Name', 'goodbids' ),
-					'gbnp-ein'        => __( 'Nonprofit EIN', 'goodbids' ),
-					'gbnp-website'    => __( 'Nonprofit Website', 'goodbids' ),
-				];
+				foreach ( $this->get_np_fields() as $key => $field ) {
+					if ( empty( $field['required'] ) || true !== $field['required'] ) {
+						continue;
+					}
 
-				foreach ( $required as $field => $label ) {
-					if ( empty( $blog[ $field ] ) ) {
+					if ( empty( $blog[ $key ] ) ) {
 						wp_die(
 							sprintf(
 								'%s %s',
-								esc_html( $label ),
+								esc_html( $field['label'] ),
 								esc_html__( 'is a required field.', 'goodbids' )
 							)
 						);
 					}
 				}
+			}
+		);
+	}
+
+	/**
+	 * Add GoodBids Nonprofit fields to the new site form
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	private function new_site_form_fields() : void {
+		add_action(
+			'network_site_new_form',
+			function() {
+				$fields = $this->get_np_fields();
+				require GOODBIDS_PLUGIN_PATH . 'views/network/new-site-fields.php';
 			}
 		);
 	}
--- client-mu-plugins/goodbids/views/network/new-site-fields.php
+++ client-mu-plugins/goodbids/views/network/new-site-fields.php
@@ -2,6 +2,8 @@
 /**
  * New Site Form Fields
  *
+ * @global $fields
+ *
  * @package GoodBids
  */
 
@@ -9,39 +11,30 @@
 <h3><?php esc_html_e( 'GoodBids Nonprofit Settings', 'goodbids' ); ?></h3>
 
 <table class="form-table" role="presentation">
-	<tr class="form-field form-required">
-		<th scope="row">
-			<label for="gbnp-legal-name">
-				<?php
-				_e( 'Nonprofit Legal Name' );
-				echo ' ' . wp_required_field_indicator(); // phpcs:ignore
-				?>
-			</label>
-		</th>
-		<td><input name="blog[gbnp-legal-name]" type="text" class="regular-text" id="gbnp-legal-name" required /></td>
-	</tr>
-	<tr class="form-field form-required">
-		<th scope="row">
-			<label for="gbnp-ein">
-				<?php
-				_e( 'Nonprofit EIN' );
-				echo ' ' . wp_required_field_indicator(); // phpcs:ignore
-				?>
-			</label>
-		</th>
-		<td><input name="blog[gbnp-ein]" type="text" class="regular-text" id="gbnp-ein" required placeholder="XX-XXXXXXX"/></td>
-	</tr>
-	<tr class="form-field form-required">
-		<th scope="row">
-			<label for="gbnp-website">
-				<?php
-				_e( 'Nonprofit Website' );
-				echo ' ' . wp_required_field_indicator(); // phpcs:ignore
-				?>
-			</label>
-		</th>
-		<td><input name="blog[gbnp-website]" type="text" class="regular-text" id="gbnp-website" required placeholder="https://" /></td>
-	</tr>
+	<?php
+	foreach ( $fields as $key => $field ) :
+		$required    = ! empty( $field['required'] ) && true === $field['required'];
+		$placeholder = $field['placeholder'] ?? '';
+
+		if ( in_array( $field['type'], [ 'text', 'url', 'email', 'tel', 'password', 'number' ], true ) ) :
+			?>
+			<tr class="form-field<?php echo $required ? ' form-required' : ''; ?>">
+				<th scope="row">
+					<label for="<?php echo esc_attr( $key ); ?>">
+						<?php
+						echo esc_html( $field['label'] );
+						if ( $required ) :
+							echo ' ' . wp_required_field_indicator(); // phpcs:ignore
+						endif;
+						?>
+					</label>
+				</th>
+				<td><input name="blog[<?php echo esc_attr( $key ); ?>]" type="<?php echo esc_attr( $field['type'] ); ?>" class="regular-text" id="<?php echo esc_attr( $key ); ?>" placeholder="<?php echo esc_attr( $placeholder ); ?>" <?php echo $required ? 'required' : ''; ?> /></td>
+			</tr>
+		<?php else : ?>
+			<p><?php esc_html_e( 'Unsupported field type.', 'goodbids' ); ?></p>
+		<?php endif; ?>
+	<?php endforeach; ?>
 </table>
 
 <div class="warning-message">
