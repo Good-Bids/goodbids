2fb346d14b1d68778f893c123df9d151480af1a9
Brian DiChiara <brian.dichiara@viget.com>
2023-12-12 15:20:21 -0600

    Create an Auth page

--- client-mu-plugins/goodbids/src/classes/plugins/WooCommerce.php
+++ client-mu-plugins/goodbids/src/classes/plugins/WooCommerce.php
@@ -32,6 +32,7 @@ class WooCommerce {
 		}
 
 		$this->configure_new_site();
+		$this->create_auth_page();
 	}
 
 	/**
@@ -62,6 +63,58 @@ class WooCommerce {
 				update_option( 'woocommerce_allow_bulk_remove_personal_data', 'yes' );
 			},
 			5
+		);
+	}
+
+	/**
+	 * Create a new Authentication Page
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	private function create_auth_page() : void {
+		add_action(
+			'goodbids_init_site',
+			function ( int $site_id ) : void {
+				// Get WooCommerce My Account Page ID
+				$account_page_id = get_option( 'woocommerce_myaccount_page_id' );
+
+				if ( ! $account_page_id ) {
+					$account_page = wpcom_vip_get_page_by_path( 'my-account' );
+
+					if ( $account_page ) {
+						$account_page_id = $account_page->ID;
+					}
+				}
+
+				$auth_page_args = [
+					'post_title'     => __( 'Authentication', 'goodbids' ),
+					'post_name'      => 'authentication',
+					'post_status'    => 'publish',
+					'post_type'      => 'page',
+					'comment_status' => 'closed',
+					'ping_status'    => 'closed',
+					'post_content'   => '<!-- wp:acf/authentication {"name":"acf/authentication","mode":"preview"} /-->',
+				];
+
+				if ( $account_page_id ) {
+					$auth_page_args['post_parent'] = $account_page_id;
+				}
+
+				$auth_page_id = wp_insert_post(
+					$auth_page_args,
+					true
+				);
+
+				if ( ! $auth_page_id ) {
+					// TODO: Log Error.
+					return;
+				}
+
+				update_option( 'woocommerce_authentication_page_id', $auth_page_id );
+			},
+			6
 		);
 	}
 }
