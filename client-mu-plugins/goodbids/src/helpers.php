b48fd4be4753059be4912f65553bf483cf0b29e7
Brian DiChiara <brian.dichiara@viget.com>
2023-12-13 10:16:00 -0600

    Wrap helper function in function_exists

--- client-mu-plugins/goodbids/src/helpers.php
+++ client-mu-plugins/goodbids/src/helpers.php
@@ -6,17 +6,19 @@
  * @package GoodBids
  */
 
-/**
- * Render the ACF block attributes
- *
- * @since 1.0.0
- *
- * @param array $block
- * @param string $addl_class
- * @param array $attr
- *
- * @return void
- */
-function block_attr( array $block, string $addl_class = '', array $attr = [] ) : void {
-	goodbids()->acf->blocks()->block_attr( $block, $addl_class, $attr );
+if ( ! function_exists( 'block_attr' ) ) {
+	/**
+	 * Render the ACF block attributes
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $block
+	 * @param string $addl_class
+	 * @param array $attr
+	 *
+	 * @return void
+	 */
+	function block_attr( array $block, string $addl_class = '', array $attr = [] ) : void {
+		goodbids()->acf->blocks()->block_attr( $block, $addl_class, $attr );
+	}
 }
