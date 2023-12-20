82aa60e6ac7de8d1775250960f05565db6fbee55
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2023-12-20 16:58:49 -0700

    [#11] setting up footer block

---
+++ client-mu-plugins/goodbids/blocks/footer/render.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Block: Main Footer
+ *
+ * @global array $block
+ *
+ * @since 1.0.0
+ * @package GoodBids
+ */
+
+
+?>
+
+
+<section <?php block_attr( $block ); ?>>
+<div class="wp-block-group alignwide has-background has-global-padding is-layout-constrained wp-block-group-is-layout-constrained">
+	<div class="wp-block-group alignwide has-global-padding is-layout-constrained wp-block-group-is-layout-constrained">
+		<div class="wp-block-group alignwide has-small-font-size is-content-justification-space-between is-nowrap is-layout-flex wp-container-core-group-layout-30 wp-block-group-is-layout-flex" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);justify-content: space-between;flex-wrap: wrap;">
+			<p>GOODBIDS Positive Auctions</p>
+			<div class="wp-block-group is-nowrap is-layout-flex wp-container-core-group-layout-29 wp-block-group-is-layout-flex">
+				<p><a href="#">Terms &amp; Conditions</a></p>
+				<p><a href="#">Privacy Policy</a></p>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
--- themes/goodbids-main/patterns/footer.php
+++ themes/goodbids-main/patterns/footer.php
@@ -7,31 +7,4 @@
  */
 ?>
 
-<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide has-background">
-	<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
-	<div class="wp-block-group alignwide">
-
-		<!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"fontSize":"small"} -->
-		<div class="wp-block-group alignwide has-small-font-size" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">
-			<!-- wp:paragraph -->
-				<p><?php esc_html_e( 'GOODBIDS positive auctions', 'goodbids-nonprofit' ); ?></p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-			<div class="wp-block-group">
-				<!-- wp:paragraph -->
-					<p><a href="#"><?php esc_html_e( 'Terms &amp; Conditions', 'goodbids-nonprofit' ); ?></a></p>
-				<!-- /wp:paragraph -->
-
-				<!-- wp:paragraph -->
-					<p><a href="#"><?php esc_html_e( 'Privacy Policy', 'goodbids-nonprofit' ); ?></a></p>
-				<!-- /wp:paragraph -->
-			</div>
-			<!-- /wp:group -->
-		</div>
-		<!-- /wp:group -->
-	</div>
-	<!-- /wp:group -->
-</div>
-<!-- /wp:group -->
+<!-- wp:acf/footer-main {"name":"acf/footer-main","mode":"preview","lock":{"move":true,"remove":true}} /-->
