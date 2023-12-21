fc94f1af6ea367b064f2e8b5bb4bea89b60515b9
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2023-12-21 16:39:53 -0700

    [#90] setting up class for privacy policy

---
+++ client-mu-plugins/goodbids/blocks/main-footer/block.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Main Footer Block
+ *
+ * @since 1.0.0
+ *
+ * @package GoodBids
+ */
+
+namespace GoodBids\Blocks;
+
+/**
+ * Class for Main Footer Block
+ *
+ * @since 1.0.0
+ */
+class MainFooter {
+
+	/**
+	 * @since 1.0.0
+	 * @var ?string
+	 */
+	private ?string $page_id = null;
+
+	/**
+	 * @since 1.0.0
+	 * @var ?string
+	 */
+	private ?string $privacy_policy_title = null;
+
+	/**
+	 * @since 1.0.0
+	 * @var ?string
+	 */
+	private ?string $privacy_policy_url = null;
+
+	/**
+	 * Initialize the block.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		$this->page_id = get_blog_option( get_main_site_id(), 'wp_page_for_privacy_policy' );
+
+		if ( is_multisite() ) {
+			switch_to_blog( get_main_site_id() );
+			$this->privacy_policy_title = get_the_title( $this->page_id );
+			$this->privacy_policy_url   = get_privacy_policy_url( $this->page_id );
+			restore_current_blog();
+		}
+	}
+
+	/**
+	 * Returns the text for the main site
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string
+	 */
+	public function get_sitename_text(): string {
+		$sitename_text = __( 'GOODBIDS Positive Auctions', 'goodbids' );
+
+		if ( is_multisite() ) {
+			$blogname      = get_blog_option( get_main_site_id(), 'blogname' );
+			$tagline       = get_blog_option( get_main_site_id(), 'blogdescription' );
+			$sitename_text = sprintf(
+				/** translators: %s: GOODBIDS Positive Auctions */
+				__( '%1$s %2$s', 'goodbids' ),
+				wp_kses_post( $blogname ),
+				wp_kses_post( $tagline )
+			);
+		}
+
+		return $sitename_text;
+	}
+
+	/**
+	 * Returns the Privacy Policy page title
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string
+	 */
+	public function get_privacy_policy_title(): string {
+		return sprintf(
+			/** translators: %s: Privacy Policy */
+			__( '%1$s', 'goodbids' ),
+			wp_kses_post( $this->privacy_policy_title ),
+		);
+	}
+
+	/**
+	 * Returns the URL for the Privacy Policy page
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string
+	 */
+	public function get_privacy_policy_url(): string {
+		return sprintf(
+			/** translators: %s: URL */
+			__( '%1$s', 'goodbids' ),
+			wp_kses_post( $this->privacy_policy_url ),
+		);
+	}
+}
--- client-mu-plugins/goodbids/blocks/main-footer/render.php
+++ client-mu-plugins/goodbids/blocks/main-footer/render.php
@@ -8,13 +8,15 @@
  * @package GoodBids
  */
 
+$main_footer = new GoodBids\Blocks\MainFooter();
+
 ?>
 
 <section <?php block_attr( $block, 'wp-block-group alignwide has-background has-global-padding is-layout-constrained wp-block-group-is-layout-constrained' ); ?>>
 	<div class="wp-block-group alignwide has-global-padding is-layout-constrained wp-block-group-is-layout-constrained">
 		<div class="wp-block-group alignwide has-small-font-size is-content-justification-space-between is-nowrap is-layout-flex wp-container-core-group-layout-30 wp-block-group-is-layout-flex" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);justify-content: space-between;flex-wrap: wrap;">
 
-			<p><?php esc_html_e( 'GOODBIDS Positive Auctions', 'goodbids' ); ?></p>
+			<p><?php echo wp_kses_post( $main_footer->get_sitename_text() ); ?></p>
 
 			<div class="wp-block-group is-nowrap is-layout-flex wp-container-core-group-layout-29 wp-block-group-is-layout-flex">
 				<?php
@@ -24,13 +26,12 @@
 					esc_html__( 'Terms & Conditions', 'goodbids' )
 				);
 				?>
-				<?php
-				printf(
-					'<p><a href="%s">%s</a></p>',
-					esc_html__( '#', 'goodbids' ),
-					esc_html__( 'Privacy Policy', 'goodbids' )
-				);
-				?>
+
+				<p>
+					<a href="<?php echo wp_kses_post( $main_footer->get_privacy_policy_url() ); ?>">
+						<?php echo wp_kses_post( $main_footer->get_privacy_policy_title() ); ?>
+					</a>
+				</p>
 			</div>
 		</div>
 	</div>
