8d0f636e7d49d81ad2f1e9c74580e9dcf6dcdcd1
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2023-12-20 17:12:25 -0700

    [#11] refactor footer

--- client-mu-plugins/goodbids/blocks/footer/render.php
+++ client-mu-plugins/goodbids/blocks/footer/render.php
@@ -8,20 +8,33 @@
  * @package GoodBids
  */
 
-
 ?>
 
-
-<section <?php block_attr( $block ); ?>>
-<div class="wp-block-group alignwide has-background has-global-padding is-layout-constrained wp-block-group-is-layout-constrained">
+<section <?php block_attr( $block, 'wp-block-group alignwide has-background has-global-padding is-layout-constrained wp-block-group-is-layout-constrained' ); ?>>
 	<div class="wp-block-group alignwide has-global-padding is-layout-constrained wp-block-group-is-layout-constrained">
 		<div class="wp-block-group alignwide has-small-font-size is-content-justification-space-between is-nowrap is-layout-flex wp-container-core-group-layout-30 wp-block-group-is-layout-flex" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);justify-content: space-between;flex-wrap: wrap;">
-			<p>GOODBIDS Positive Auctions</p>
+			<?php
+			printf(
+				'<p>%s</p>',
+				esc_html__( 'GOODBIDS Positive Auctions', 'goodbids' )
+			);
+			?>
 			<div class="wp-block-group is-nowrap is-layout-flex wp-container-core-group-layout-29 wp-block-group-is-layout-flex">
-				<p><a href="#">Terms &amp; Conditions</a></p>
-				<p><a href="#">Privacy Policy</a></p>
+				<?php
+				printf(
+					'<p><a href="%s">%s</a></p>',
+					esc_html__( '#', 'goodbids' ),
+					esc_html__( 'Terms & Conditions', 'goodbids' )
+				);
+				?>
+				<?php
+				printf(
+					'<p><a href="%s">%s</a></p>',
+					esc_html__( '#', 'goodbids' ),
+					esc_html__( 'Privacy Policy', 'goodbids' )
+				);
+				?>
 			</div>
 		</div>
 	</div>
-</div>
 </section>
