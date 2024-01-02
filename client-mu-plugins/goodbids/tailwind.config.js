ff0aead22c271437e0ffe09a9fd7edee633e7e8f
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-01-02 15:49:42 -0700

    [#127] adding in custom TW styles

--- client-mu-plugins/goodbids/tailwind.config.js
+++ client-mu-plugins/goodbids/tailwind.config.js
@@ -9,6 +9,20 @@ export default {
 		"./views/**/**/*.{php,html}",
 	],
 	theme: {
+		colors: {
+			//Colors are set in the wordpress theme.json
+			'base': 'var(--wp--preset--color--base)',
+			'base-2': 'var(--wp--preset--color--base-2)',
+			'base-3': 'var(--wp--preset--color--base-3)',
+			'contrast': 'var(--wp--preset--color--contrast)',
+			'contrast-2': 'var(--wp--preset--color--contrast-2)',
+			'contrast-3': 'var(--wp--preset--color--contrast-3)',
+			'contrast-4': 'var(--wp--preset--color--contrast-4)',
+			'accent': 'var(--wp--preset--color--accent)',
+			'accent-2': 'var(--wp--preset--color--accent-2)',
+			'accent-3': 'var(--wp--preset--color--accent-3)',
+			'accent-4': 'var(--wp--preset--color--accent-4)',
+		},
 		borderRadius: {
 			DEFAULT: '30px',
 		}
--- client-mu-plugins/goodbids/views/patterns/charity-interest-form.php
+++ client-mu-plugins/goodbids/views/patterns/charity-interest-form.php
@@ -14,26 +14,26 @@
 	<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|10","left":"var:preset|spacing|10","top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
 	<div class="wp-block-group" style="padding-top:0;padding-right:var(--wp--preset--spacing--10);padding-bottom:0;padding-left:var(--wp--preset--spacing--10)">
 		<!-- wp:heading {"style":{"typography":{"textTransform":"none"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base"} -->
-		<h2 class="wp-block-heading has-base-color has-text-color has-link-color" style="text-transform:none">Nice to meet you!</h2>
+		<h2 class="wp-block-heading has-base-color has-text-color has-link-color" style="text-transform:none"><?php esc_html_e('Nice to meet you!', 'goodbids'); ?></h2>
 		<!-- /wp:heading -->
 
 		<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"textColor":"base-2"} -->
-		<p class="has-base-2-color has-text-color has-link-color">Share your email below so we can contact you. We usually follow up within two workdays.</p>
+		<p class="has-base-2-color has-text-color has-link-color"><?php esc_html_e('Share your email below so we can contact you. We usually follow up within two workdays.', 'goodbids'); ?></p>
 		<!-- /wp:paragraph -->
 	</div>
 	<!-- /wp:group -->
 
 	<!-- wp:jetpack/contact-form {"subject":"[goodbids.go-vip.net] GOODBIDS for Charities","to":"claire.eisinger@viget.com","className":"is-style-default","textColor":"base","style":{"spacing":{"padding":{"top":"16px","right":"16px","bottom":"16px","left":"16px"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}}} -->
 	<div class="wp-block-jetpack-contact-form is-style-default has-base-color has-text-color has-link-color has-base-color has-text-color has-link-color" style="padding-top:16px;padding-right:16px;padding-bottom:16px;padding-left:16px">
-		<!-- wp:jetpack/field-name {"label":"Primary Contact Name","required":true,"requiredText":"(required)","borderRadius":30,"borderWidth":2,"lineHeight":1,"inputColor":"#FFFFFF","fieldBackgroundColor":"#0A3624","borderColor":"#70FF8F"} /-->
+		<!-- wp:jetpack/field-name {"label":"Primary Contact Name","required":true,"requiredText":"(required)","borderRadius":30,"borderWidth":2,"lineHeight":1,"inputColor":"#FFFFFF","fieldBackgroundColor":"#0A3624","borderColor":"#D9FFD2","className":"placeholder:text-base focus:outline-contrast-4"} /-->
 
-		<!-- wp:jetpack/field-email {"label":"Primary Contact Email","required":true,"requiredText":"(required)","placeholder":"E.g. email@email.xyz","borderRadius":30,"borderWidth":2,"lineHeight":1,"inputColor":"#FFFFFF","fieldBackgroundColor":"#0A3624","borderColor":"#70FF8F"} /-->
+		<!-- wp:jetpack/field-email {"label":"Primary Contact Email","required":true,"requiredText":"(required)","placeholder":"E.g. email@email.xyz","borderRadius":30,"borderWidth":2,"lineHeight":1,"inputColor":"#FFFFFF","fieldBackgroundColor":"#0A3624","borderColor":"#D9FFD2","className":"placeholder:text-base focus:outline-contrast-4"} /-->
 
-		<!-- wp:jetpack/field-text {"label":"Charity Legal Name","required":true,"requiredText":"(required)","placeholder":"E.g. My Charity, Inc.","borderRadius":30,"borderWidth":2,"lineHeight":1,"inputColor":"#FFFFFF","fieldBackgroundColor":"#0A3624","borderColor":"#70FF8F"} /-->
+		<!-- wp:jetpack/field-text {"label":"Charity Legal Name","required":true,"requiredText":"(required)","placeholder":"E.g. My Charity, Inc.","borderRadius":30,"borderWidth":2,"lineHeight":1,"inputColor":"#FFFFFF","fieldBackgroundColor":"#0A3624","borderColor":"#D9FFD2","className":"placeholder:text-base focus:outline-contrast-4"} /-->
 
-		<!-- wp:jetpack/field-text {"label":"Charity EIN","required":true,"requiredText":"(required)","placeholder":"XX-XXXXXXX","borderRadius":30,"borderWidth":2,"lineHeight":1,"inputColor":"#FFFFFF","fieldBackgroundColor":"#0A3624","borderColor":"#70FF8F"} /-->
+		<!-- wp:jetpack/field-text {"label":"Charity EIN","required":true,"requiredText":"(required)","placeholder":"XX-XXXXXXX","borderRadius":30,"borderWidth":2,"lineHeight":1,"inputColor":"#FFFFFF","fieldBackgroundColor":"#0A3624","borderColor":"#D9FFD2","className":"placeholder:text-base focus:outline-contrast-4"} /-->
 
-		<!-- wp:jetpack/field-url {"label":"Charity Website","required":true,"requiredText":"(required)","placeholder":"https://","borderRadius":30,"borderWidth":2,"lineHeight":1,"inputColor":"#FFFFFF","fieldBackgroundColor":"#0A3624","borderColor":"#70FF8F"} /-->
+		<!-- wp:jetpack/field-url {"label":"Charity Website","required":true,"requiredText":"(required)","placeholder":"https://","borderRadius":30,"borderWidth":2,"lineHeight":1,"inputColor":"#FFFFFF","fieldBackgroundColor":"#0A3624","borderColor":"#D9FFD2","className":"placeholder:text-base focus:outline-contrast-4"} /-->
 
 		<!-- wp:jetpack/button {"element":"button","text":"Submit","textColor":"contrast","backgroundColor":"contrast-4","lock":{"remove":true}} /-->
 	</div>
