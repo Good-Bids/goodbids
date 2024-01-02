77e7cc23579a0fda0a53c1044ba8bad96ac8530e
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-01-02 13:27:17 -0700

    [#90] updating esc_html

--- client-mu-plugins/goodbids/blocks/main-footer/render.php
+++ client-mu-plugins/goodbids/blocks/main-footer/render.php
@@ -16,20 +16,20 @@ $main_footer = new GoodBids\Blocks\MainFooter();
 	<div class="wp-block-group alignwide has-global-padding is-layout-constrained wp-block-group-is-layout-constrained">
 		<div class="wp-block-group alignwide has-small-font-size is-content-justification-space-between is-nowrap is-layout-flex wp-container-core-group-layout-30 wp-block-group-is-layout-flex" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);justify-content: space-between;flex-wrap: wrap;">
 
-			<p><?php echo wp_kses_post( $main_footer->get_sitename_text() ); ?></p>
+			<p><?php echo esc_html_e( $main_footer->get_sitename_text(), 'goodbids' ); ?></p>
 
 			<div class="wp-block-group is-nowrap is-layout-flex wp-container-core-group-layout-29 wp-block-group-is-layout-flex">
 				<?php
 				printf(
 					'<p><a href="%s">%s</a></p>',
-					esc_html__( '#', 'goodbids' ),
-					esc_html__( 'Terms & Conditions', 'goodbids' )
+					esc_html_e( '#', 'goodbids' ),
+					esc_html_e( 'Terms & Conditions', 'goodbids' )
 				);
 				?>
 
 				<p>
-					<a href="<?php echo wp_kses_post( $main_footer->get_privacy_policy_url() ); ?>">
-						<?php echo wp_kses_post( $main_footer->get_privacy_policy_title() ); ?>
+					<a href="<?php echo esc_url( $main_footer->get_privacy_policy_url() ); ?>">
+						<?php echo esc_html_e( $main_footer->get_privacy_policy_title(), 'goodbids' ); ?>
 					</a>
 				</p>
 			</div>
