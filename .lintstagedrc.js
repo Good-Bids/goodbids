0b505e500aa45dba748fac69db5fa6bf9d8f8061
Nick Telsan <nick.telsan@viget.com>
2024-01-04 12:28:17 -0500

    [#93] Package Adjustments
    - Removes toplevel package.json
    - Moves configuration files down into the goodbids plugin
    - Adjusts config to handle new structure

--- .lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  './client-mu-plugins/goodbids/assets/**/*.{js,jsx,ts,tsx}': [
-    'eslint',
-    'prettier --check ./client-mu-plugins/goodbids/src/**/*.{js,jsx,ts,tsx}',
-  ],
-  './client-mu-plugins/goodbids/assets/**/*.css': [
-    'prettier --check ./client-mu-plugins/goodbids/src/**/*.css',
-  ],
-};
