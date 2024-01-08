3cc82ea1bce5575389eef4d27383a43eddca6479
Nick <nick.telsan@viget.com>
2024-01-08 10:33:10 -0500

    [#95] Countdown Timer (#148)
    - Adds countdown timer component and functions
    - Adjusts initial state for demo
    - Adjusts font styles
    - Fixes gitignore build

---
+++ client-mu-plugins/goodbids/src/blocks/bidding/client.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { render } from '@wordpress/element';
+
+render(<h1>Hello World</h1>, document.getElementById('goodbids-bidding'));
--- client-mu-plugins/goodbids/src/blocks/bidding/edit.tsx
+++ client-mu-plugins/goodbids/src/blocks/bidding/edit.tsx
@@ -2,9 +2,8 @@
  * WordPress Dependencies
  */
 import React from 'react';
-import { useBlockProps } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
-import { Driver } from './components/driver';
+import { ClockIcon } from './components/clock-icon';
 
 const Edit = ({ setAttributes }) => {
 	const auctionId = useSelect((select) => {
@@ -12,15 +11,37 @@ const Edit = ({ setAttributes }) => {
 		return select('core/editor').getCurrentPostId();
 	}, []);
 
-	const blockProps = useBlockProps({
-		className: 'p-8',
-	});
-
 	setAttributes({ auctionId });
 
 	return (
-		<div {...blockProps}>
-			<Driver />
+		<div className="w-full">
+			<div className="grid grid-cols-3 gap-5 my-4">
+				<div className="flex flex-col text-center">
+					<p className="m-0 font-thin uppercase has-x-small-font-size">
+						BIDS
+					</p>
+					<p className="m-1 font-extrabold">{0}</p>
+				</div>
+				<div className="flex flex-col text-center">
+					<p className="m-0 font-thin uppercase has-x-small-font-size">
+						RAISED
+					</p>
+					<p className="m-1 font-extrabold">$0</p>
+				</div>
+				<div className="flex flex-col text-center">
+					<p className="m-0 font-thin uppercase has-x-small-font-size">
+						LAST BID
+					</p>
+					<p className="m-1 font-extrabold">$0</p>
+				</div>
+			</div>
+			<div className="flex items-center gap-3 px-4">
+				<ClockIcon />
+
+				<span className="text-lg">
+					<b>Auction has closed.</b>
+				</span>
+			</div>
 		</div>
 	);
 };
