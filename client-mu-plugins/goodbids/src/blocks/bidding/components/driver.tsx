5e05c95e278e3c04422e65b7bed030f9ef334a90
Nick <nick.telsan@viget.com>
2024-01-08 00:59:28 -0500

    [#94] Metrics but WP (#155)
    - Adds driver and metrics component
    - Adjusts Tailwind content paths
    - Adds inputs for new components
    - Adds script to handle components in dev mode
    - Moves metrics into the WP block

---
+++ client-mu-plugins/goodbids/src/blocks/bidding/components/driver.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { getInitialState } from '../utils/get-initial-state';
+import { Metrics } from './metrics';
+
+export function Driver() {
+	const initialState = getInitialState();
+
+	return (
+		<div className="w-full">
+			<Metrics
+				blocks={[
+					{ type: 'bids', value: initialState.bids },
+					{ type: 'raised', value: initialState.raised },
+					{ type: 'last-bid', value: initialState.lastBid },
+				]}
+			/>
+		</div>
+	);
+}
