caf87af5689713af509ad4ef3e2fd34f1e7ceb98
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-01-09 14:02:31 -0700

    [#143] Refactor - to make it more flexible
    [#143] minor refactor
    [#143] refactoring foreach array to collection

--- client-mu-plugins/goodbids/blocks/all-auctions/block.php
+++ client-mu-plugins/goodbids/blocks/all-auctions/block.php
@@ -10,6 +10,7 @@
 namespace GoodBids\Blocks;
 
 use GoodBids\Plugins\ACF\ACFBlock;
+use Illuminate\Support\Collection;
 
 /**
  * Class for All Auctions Block
@@ -19,44 +20,55 @@ use GoodBids\Plugins\ACF\ACFBlock;
 class AllAuctions extends ACFBlock {
 
 	/**
-	 * Returns an array of all active auctions
+	 * Returns an array of all active auctions across all sites
 	 *
 	 * @since 1.0.0
 	 *
 	 * @return array
 	 */
-	public function get_all_auctions(): array {
-		$all_auctions = [];
-		foreach ( get_sites() as $nonprofit ) {
-			$nonprofit_id = get_object_vars( $nonprofit )['blog_id'];
-
-			$all_auctions[] = $this->get_site_auctions( $nonprofit_id );
-		}
-
-		return array_filter( array_merge( ...$all_auctions ) );
+	public function get_all_site_auctions(): array {
+		return Collection::make( get_sites() )
+			->flatMap(
+				function ( $site ) {
+					$site_id = get_object_vars( $site )['blog_id'];
+					return $this->get_site_auctions( $site_id );
+				}
+			)
+			->filter()
+			->all();
 	}
 
 	/**
-	 * Returns an array of all active auctions
+	 * Returns an array of all active auctions for a given site
 	 *
 	 * @since 1.0.0
 	 *
-	 * @return mixed
+	 * @param int $site_id
+	 *
+	 * @return array
 	 */
-	private function get_site_auctions( $nonprofit_id ): mixed {
-		$args = [
-			'post_type' => 'gb-auction',
+	private function get_site_auctions( int $site_id ): array {
+		// Start by switching to blog and get all auctions
+		switch_to_blog( $site_id );
+		$args     = [
+			'post_type'      => goodbids()->auctions->get_post_type(),
+			'post_status'    => 'publish',
+			'posts_per_page' => -1,
+			'fields'         => 'ids',
 		];
-
-		switch_to_blog( $nonprofit_id );
-		$auctions = get_posts( $args );
-		foreach ( $auctions as $auction ) {
-			if ( has_post_thumbnail( $auction->ID ) ) {
-				$auction->img = get_the_post_thumbnail( $auction->ID, 'woocommerce_thumbnail' );
-			}
-		}
+		$auctions = new \WP_Query( $args );
 		restore_current_blog();
 
-		return $auctions;
+		if ( ! $auctions->have_posts() ) {
+			return [];
+		}
+		return collect( $auctions->posts )->map(
+			function ( $post_id ) use ( $site_id ) {
+				return [
+					'post_id' => $post_id,
+					'site_id' => $site_id,
+				];
+			}
+		)->all();
 	}
 }
