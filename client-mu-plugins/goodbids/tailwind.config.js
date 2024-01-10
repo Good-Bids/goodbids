3fd3d0ff8e646de39e58b14a02e6333c1cbf938b
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-01-10 12:32:45 -0700

    [#127] styling the interest form after submit

--- client-mu-plugins/goodbids/tailwind.config.js
+++ client-mu-plugins/goodbids/tailwind.config.js
@@ -11,21 +11,53 @@ export default {
 	theme: {
 		colors: {
 			//Colors are set in the wordpress theme.json
-			'base': 'var(--wp--preset--color--base)',
+			base: 'var(--wp--preset--color--base)',
 			'base-2': 'var(--wp--preset--color--base-2)',
 			'base-3': 'var(--wp--preset--color--base-3)',
-			'contrast': 'var(--wp--preset--color--contrast)',
+			contrast: 'var(--wp--preset--color--contrast)',
 			'contrast-2': 'var(--wp--preset--color--contrast-2)',
 			'contrast-3': 'var(--wp--preset--color--contrast-3)',
 			'contrast-4': 'var(--wp--preset--color--contrast-4)',
-			'accent': 'var(--wp--preset--color--accent)',
+			accent: 'var(--wp--preset--color--accent)',
 			'accent-2': 'var(--wp--preset--color--accent-2)',
 			'accent-3': 'var(--wp--preset--color--accent-3)',
 			'accent-4': 'var(--wp--preset--color--accent-4)',
+			transparent: 'transparent',
 		},
 		borderRadius: {
 			DEFAULT: '30px',
 		},
+		borderWidth: {
+			DEFAULT: '1px',
+			0: '0',
+		},
 	},
-	plugins: [],
+	plugins: [
+		({ addComponents }) => {
+			//set up btn classes
+			const buttonBase = {
+				'@apply rounded py-3 px-5 border border-solid border-transparent no-underline':
+					{},
+			};
+
+			addComponents({
+				//set up btn classes
+				'.btn-fill': {
+					...buttonBase,
+					'@apply bg-contrast text-base-2 hover:bg-base-2 hover:text-contrast focus:bg-base-2 focus:text-contrast':
+						{},
+				},
+				'.btn-fill-secondary': {
+					...buttonBase,
+					'@apply bg-base-2 text-contrast hover:bg-contrast hover:text-base-2 focus:bg-contrast focus:text-base-2':
+						{},
+				},
+				'.btn-outline': {
+					...buttonBase,
+					'@apply bg-transparent text-contrast border-contrast hover:bg-contrast hover:border-contrast hover:text-contrast-4 focus:bg-contrast focus:border-contrast focus:text-contrast-4':
+						{},
+				},
+			});
+		},
+	],
 };
--- client-mu-plugins/goodbids/views/patterns/nonprofit-interest-form.php
+++ client-mu-plugins/goodbids/views/patterns/nonprofit-interest-form.php
@@ -8,8 +8,8 @@
 
 ?>
 
-<!-- wp:group {"style":{"border":{"radius":"16px"},"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"backgroundColor":"contrast","textColor":"base-2","layout":{"type":"constrained","wideSize":"","contentSize":""}} -->
-<div class="wp-block-group has-base-2-color has-contrast-background-color has-text-color has-background has-link-color"
+<!-- wp:group {"style":{"border":{"radius":"16px"},"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"backgroundColor":"contrast","textColor":"base-2","className":"nonprofit-interest-form","layout":{"type":"constrained","wideSize":"","contentSize":""}} -->
+<div class="wp-block-group nonprofit-interest-form has-base-2-color has-contrast-background-color has-text-color has-background has-link-color"
 	style="border-radius:16px;margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50);padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--20)">
 	<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|10","left":"var:preset|spacing|10","top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
 	<div class="wp-block-group" style="padding-top:0;padding-right:var(--wp--preset--spacing--10);padding-bottom:0;padding-left:var(--wp--preset--spacing--10)">
