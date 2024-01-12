f5891b31f7e1146b759d4487c88125145afe9cb4
Joshua Pease <joshua.pease@viget.com>
2024-01-12 15:17:58 -0800

    [n/a] Adds example vip-env-vars file and docs about using it when setting up local dev.

---
+++ client-mu-plugins/vip-env-vars.exampe.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Copy and paste this file, renaming it to "vip-env-vars.local.php"
+ *
+ * Fill in any TODO env vars
+ *
+ * Do not store sensitive data in this example file.
+ * Feel free to pre-fill environment variables that contain
+ * non-sensitive data, but env vars that contain things like
+ * passwords and api keys should be left as TODOs
+ */
+
+//
+defined( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_URL_LOCAL' ) || define( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_URL_LOCAL', 'http://localhost:3000' );
+defined( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_URL_DEVELOP' ) || define( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_URL_DEVELOP', 'https://goodbids-node-develop.go-vip.net' );
+defined( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_URL_PRODUCTION' ) || define( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_URL_PRODUCTION', 'https://goodbids-node.go-vip.net' );
+
+// TODO
+// defined( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_API_KEY' ) || define( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_API_KEY', 'YOUR_VALUE_HERE' );
