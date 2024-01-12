86030877629d4f60688b6b67aa3d206007755ccf
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-01-12 12:43:14 -0700

    [#174] Terms and Conditions page (#196)
    * [#174] setting up new T&C pattern
    
    * [#174] Linking T&C page in the footer
    
    * [#174] Removing the duplicate page title
    
    * [#174] updating ?string and esc_html

--- client-mu-plugins/goodbids/blocks/main-footer/block.php
+++ client-mu-plugins/goodbids/blocks/main-footer/block.php
@@ -20,6 +20,18 @@ class MainFooter {
 	 * @since 1.0.0
 	 * @var ?string
 	 */
+	private ?string $terms_conditions_title = null;
+
+	/**
+	 * @since 1.0.0
+	 * @var ?string
+	 */
+	private ?string $terms_conditions_url = null;
+
+	/**
+	 * @since 1.0.0
+	 * @var ?string
+	 */
 	private ?string $privacy_policy_title = null;
 
 	/**
@@ -34,26 +46,35 @@ class MainFooter {
 	 * @since 1.0.0
 	 */
 	public function __construct() {
-		$this->setup_privacy_policy();
+		$this->setup_footer_links();
 	}
 
 	/**
-	 * Gets the title and url of the privacy policy page
+	 * Gets the title and url of the terms and conditions and privacy policy page
 	 *
 	 * @since 1.0.0
 	 *
 	 * @return void
 	 */
-	private function setup_privacy_policy(): void {
+	private function setup_footer_links(): void {
 		if ( ! is_multisite() ) {
 			return;
 		}
 
 		switch_to_blog( get_main_site_id() );
+		$privacy_policy_id   = get_option( 'wp_page_for_privacy_policy' );
+		$terms_conditions_id = wc_terms_and_conditions_page_id();
 
-		$page_id = get_option( 'wp_page_for_privacy_policy' );
-		$this->privacy_policy_title = get_the_title( $page_id );
-		$this->privacy_policy_url   = get_privacy_policy_url( $page_id );
+		if ( $privacy_policy_id ) {
+			$this->privacy_policy_title = get_the_title( $privacy_policy_id );
+			$this->privacy_policy_url   = get_privacy_policy_url( $privacy_policy_id );
+		}
+
+		if ( $terms_conditions_id ) {
+			$this->terms_conditions_title = get_the_title( $terms_conditions_id );
+			$this->terms_conditions_url   = get_page_link( $terms_conditions_id );
+		}
+
 		restore_current_blog();
 	}
 
@@ -83,9 +104,9 @@ class MainFooter {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @return string
+	 * @return ?string
 	 */
-	public function get_privacy_policy_title(): string {
+	public function get_privacy_policy_title(): ?string {
 		return $this->privacy_policy_title;
 	}
 
@@ -94,9 +115,31 @@ class MainFooter {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @return string
+	 * @return ?string
 	 */
-	public function get_privacy_policy_url(): string {
+	public function get_privacy_policy_url(): ?string {
 		return $this->privacy_policy_url;
+	}
+
+	/**
+	 * Returns the Terms and Conditions page title
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return ?string
+	 */
+	public function get_terms_conditions_title(): ?string {
+		return $this->terms_conditions_title;
+	}
+
+	/**
+	 * Returns the URL for the Terms and Conditions page
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return ?string
+	 */
+	public function get_terms_conditions_url(): ?string {
+		return $this->terms_conditions_url;
 	}
 }
