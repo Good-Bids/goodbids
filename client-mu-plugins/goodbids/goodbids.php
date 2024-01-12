a53d9f02aea7796d8ee7fb54db3d43e02b47020b
Joshua Pease <joshua.pease@viget.com>
2024-01-12 15:18:13 -0800

    [n/a] Adds `dd()` (dump & die) method.

--- client-mu-plugins/goodbids/goodbids.php
+++ client-mu-plugins/goodbids/goodbids.php
@@ -27,6 +27,22 @@ if ( file_exists( GOODBIDS_PLUGIN_PATH . 'vendor/autoload.php' ) ) {
 	require GOODBIDS_PLUGIN_PATH . 'vendor/autoload.php';
 }
 
+// Helpful Dump & Die method for printing debug data to the screen
+// https://gist.github.com/james2doyle/abfbd4dc5754712bac022faf4e2881a6
+if (!function_exists('dd')) {
+	function dd($data)
+	{
+		ini_set("highlight.comment", "#969896; font-style: italic");
+		ini_set("highlight.default", "#FFFFFF");
+		ini_set("highlight.html", "#D16568");
+		ini_set("highlight.keyword", "#7FA3BC; font-weight: bold");
+		ini_set("highlight.string", "#F2C47E");
+		$output = highlight_string("<?php\n\n" . var_export($data, true), true);
+		echo "<div style=\"background-color: #1C1E21; padding: 1rem\">{$output}</div>";
+		die();
+	}
+}
+
 /**
  * Plugin Function Shortcode.
  *
