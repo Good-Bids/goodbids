ae34822ab360d7221b44767bbba89b5c5432295a
Nick <nick.telsan@viget.com>
2024-01-16 12:18:46 -0500

    [n/a] React Container Debug (#199)
    - Adds event listener

--- client-mu-plugins/goodbids/src/blocks/bidding/view.tsx
+++ client-mu-plugins/goodbids/src/blocks/bidding/view.tsx
@@ -2,4 +2,18 @@ import * as React from 'react';
 import { render } from '@wordpress/element';
 import { Driver } from './components/Driver';
 
-render(<Driver />, document.getElementById('goodbids-bidding'));
+function renderReact() {
+	const root = document.getElementById('goodbids-bidding');
+
+	if (root) {
+		render(<Driver />, root);
+	}
+}
+
+if (document.readyState === 'loading') {
+	// Loading hasn't finished yet
+	document.addEventListener('DOMContentLoaded', renderReact);
+} else {
+	// `DOMContentLoaded` has already fired
+	renderReact();
+}
