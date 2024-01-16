6cb93f15b937ff069d3f982d09ebe7df01c8b403
Nick Telsan <nick.telsan@viget.com>
2024-01-16 00:28:08 -0500

    [n/a] React Container Debug
    - Adds window.onload to ensure dom elements exist before rendering React

--- client-mu-plugins/goodbids/src/blocks/bidding/view.tsx
+++ client-mu-plugins/goodbids/src/blocks/bidding/view.tsx
@@ -2,4 +2,6 @@ import * as React from 'react';
 import { render } from '@wordpress/element';
 import { Driver } from './components/Driver';
 
-render(<Driver />, document.getElementById('goodbids-bidding'));
+window.onload = () => {
+	render(<Driver />, document.getElementById('goodbids-bidding'));
+};
