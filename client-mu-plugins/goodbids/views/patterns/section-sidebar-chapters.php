a0027e5385de453263f6f70381d5064420a371c1
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-01-16 19:45:33 -0700

    [#174] adding terms and condition page pattern

--- client-mu-plugins/goodbids/views/patterns/section-sidebar-chapters.php
+++ client-mu-plugins/goodbids/views/patterns/section-sidebar-chapters.php
@@ -8,108 +8,14 @@
 
 ?>
 
-<!-- wp:group {"align":"wide","layout":{"type":"constrained","justifyContent":"left"}} -->
-<div class="wp-block-group alignwide">
-	<!-- wp:post-title {"level":1,"align":"wide"} /-->
-</div>
-<!-- /wp:group -->
-
 <!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide">
 	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"0"},"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|30"}}}} -->
 	<div class="wp-block-columns alignwide" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--30)">
 		<!-- wp:column -->
 		<div class="wp-block-column">
-			<!-- wp:heading -->
-			<h2 class="wp-block-heading"><?php esc_html_e( 'This Section', 'goodbids' ); ?></h2>
-			<!-- /wp:heading -->
-		</div>
-		<!-- /wp:column -->
-
-		<!-- wp:column -->
-		<div class="wp-block-column">
-			<!-- wp:heading {"level":3} -->
-			<h3 class="wp-block-heading"><?php esc_html_e( 'This Chapter', 'goodbids' ); ?></h3>
-			<!-- /wp:heading -->
-
-			<!-- wp:paragraph -->
-			<p><?php esc_html_e( 'We’re turning charity auctions and fundraising upside down and inside out.', 'goodbids' ); ?></p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:paragraph -->
-			<p><?php esc_html_e( 'A GOODBIDS positive auction has a great prize, and you can submit bids, but the big difference is that every bid is a non-refundable donation to a charity you care about.', 'goodbids' ); ?></p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:paragraph -->
-			<p><?php esc_html_e( 'That means that great prizes end up going for really low bids. And charities end up raising more money at the same time.', 'goodbids' ); ?></p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:heading {"level":3} -->
-			<h3 class="wp-block-heading"><?php esc_html_e( 'This Chapter', 'goodbids' ); ?></h3>
-			<!-- /wp:heading -->
-
-			<!-- wp:paragraph -->
-			<p><?php esc_html_e( 'All you need to do is find an auction you like and place a bid. The money goes directly to the charity and you’ll get a receipt for your tax deductible donation.', 'goodbids' ); ?></p>
-			<!-- /wp:paragraph -->
-		</div>
-		<!-- /wp:column -->
-	</div>
-	<!-- /wp:columns -->
-</div>
-<!-- /wp:group -->
-
-<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide">
-	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"0"},"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|30"}}}} -->
-	<div class="wp-block-columns alignwide" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--30)">
-		<!-- wp:column -->
-		<div class="wp-block-column">
-			<!-- wp:heading -->
-			<h2 class="wp-block-heading"><?php esc_html_e( 'This Section', 'goodbids' ); ?></h2>
-			<!-- /wp:heading -->
-		</div>
-		<!-- /wp:column -->
-
-		<!-- wp:column -->
-		<div class="wp-block-column">
-			<!-- wp:heading {"level":3} -->
-			<h3 class="wp-block-heading"><?php esc_html_e( 'This Chapter', 'goodbids' ); ?></h3>
-			<!-- /wp:heading -->
-
-			<!-- wp:paragraph -->
-			<p><?php esc_html_e( 'We’re turning charity auctions and fundraising upside down and inside out.', 'goodbids' ); ?></p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:paragraph -->
-			<p><?php esc_html_e( 'A GOODBIDS positive auction has a great prize, and you can submit bids, but the big difference is that every bid is a non-refundable donation to a charity you care about.', 'goodbids' ); ?></p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:paragraph -->
-			<p><?php esc_html_e( 'That means that great prizes end up going for really low bids. And charities end up raising more money at the same time.', 'goodbids' ); ?></p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:heading {"level":3} -->
-			<h3 class="wp-block-heading"><?php esc_html_e( 'This Chapter', 'goodbids' ); ?></h3>
-			<!-- /wp:heading -->
-
-			<!-- wp:paragraph -->
-			<p><?php esc_html_e( 'All you need to do is find an auction you like and place a bid. The money goes directly to the charity and you’ll get a receipt for your tax deductible donation.', 'goodbids' ); ?></p>
-			<!-- /wp:paragraph -->
-		</div>
-		<!-- /wp:column -->
-	</div>
-	<!-- /wp:columns -->
-</div>
-<!-- /wp:group -->
-
-<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide">
-	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"0"},"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|30"}}}} -->
-	<div class="wp-block-columns alignwide" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--30)">
-		<!-- wp:column -->
-		<div class="wp-block-column">
-			<!-- wp:heading -->
-			<h2 class="wp-block-heading"><?php esc_html_e( 'This Section', 'goodbids' ); ?></h2>
+			<!-- wp:heading {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} -->
+			<h2 class="wp-block-heading" style="margin-bottom:var(--wp--preset--spacing--50)"><?php esc_html_e( 'This Section', 'goodbids' ); ?></h2>
 			<!-- /wp:heading -->
 		</div>
 		<!-- /wp:column -->
---
+++ client-mu-plugins/goodbids/views/patterns/template-terms-conditions.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Pattern: Terms and Conditions Template
+ *
+ * @since 1.0.0
+ * @package GoodBids
+ */
+
+?>
+
+<!-- wp:group {"align":"wide","layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group alignwide">
+	<!-- wp:post-title {"level":1,"align":"wide"} /-->
+</div>
+<!-- /wp:group -->
+
+<?php foreach ( range( 0, 2 ) as $i ) : ?>
+	<!-- wp:pattern {"slug":"goodbids/section-sidebar-chapters"} /-->
+<?php endforeach; ?>
