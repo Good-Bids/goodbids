4b7b3d69427e12b697d8904c6e9126439ffc02e8
Brian DiChiara <brian.dichiara@viget.com>
2024-01-18 10:44:47 -0600

    [N/A] Fix MainFooter Block

--- client-mu-plugins/goodbids/blocks/main-footer/render.php
+++ client-mu-plugins/goodbids/blocks/main-footer/render.php
@@ -8,10 +8,8 @@
  * @package GoodBids
  */
 
-$main_footer = new GoodBids\Blocks\MainFooter();
-
+$main_footer = new GoodBids\Blocks\MainFooter( $block );
 ?>
-
 <section <?php block_attr( $block, 'wp-block-group alignwide has-background has-global-padding is-layout-constrained wp-block-group-is-layout-constrained' ); ?>>
 	<div class="wp-block-group alignwide has-global-padding is-layout-constrained wp-block-group-is-layout-constrained">
 		<div class="wp-block-group alignwide has-small-font-size is-content-justification-space-between is-nowrap is-layout-flex wp-container-core-group-layout-30 wp-block-group-is-layout-flex" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);justify-content: space-between;flex-wrap: wrap;">
