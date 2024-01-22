a2674a760f99b2820b88398b3a5363ae3d282670
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-01-22 16:03:11 -0700

    [#236] adding in copy for email

--- client-mu-plugins/goodbids/views/woocommerce/emails/auction-watchers-live.php
+++ client-mu-plugins/goodbids/views/woocommerce/emails/auction-watchers-live.php
@@ -13,6 +13,32 @@ defined( 'ABSPATH' ) || exit;
 do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
 
+<?php /* translators: %s: Customer username */ ?>
+<p><?php printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $user_login ) ); ?></p>
+
+<p>
+	<?php
+		/* translators: %1$s: Auction title, %2$s: Site title, %3$s: Auction Goal, %4$s: Auction URL  */
+		printf(
+			esc_html__( 'The %1$s auction you are watching is now open for bidding. Visit the auction to support %2$s goal of raising %3$s and place a bid for your chance to win the %4$s!', 'goodbids' ),
+			esc_html( get_the_title( $auction_id ) ),
+			esc_html( $blogname ),
+			esc_html( $auction_goal ),
+			esc_html( get_the_permalink( $auction_id ) )
+		);
+		?>
+</p>
+
+<p>
+	<?php
+	/* translators: %1$s: Auction Starting Date */
+	printf(
+		esc_html__( 'Bidding starts at %1$s and the first five paid bidders on this auction will earn a Free Bid.', 'goodbids' ),
+		// TODO update to starting date
+	);
+	?>
+</p>
+
 
 <?php
 /** * Show user-defined additional content - this is set in each email's settings. */ if ( $additional_content ) {
