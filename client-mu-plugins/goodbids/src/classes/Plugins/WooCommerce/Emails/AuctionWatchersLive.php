522adf40aee1a092c34848ec93982963b9f4f7b2
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-01-30 15:00:36 -0700

    [#236] Auction Live Watcher (#261)
    * [#236] adding in copy for email
    
    * [#236] adding in content for email, fixing reset password style
    
    * [#236] adding custom style
    
    * [#236] setting up user vars and plan email
    
    * [#236] refactor vars to make it easy to update later
    
    * [#236] updating from PR review
    
    * [#236] adding getters for auction info - refactor

--- client-mu-plugins/goodbids/src/classes/Plugins/WooCommerce/Emails/AuctionWatchersLive.php
+++ client-mu-plugins/goodbids/src/classes/Plugins/WooCommerce/Emails/AuctionWatchersLive.php
@@ -11,6 +11,7 @@ namespace GoodBids\Plugins\WooCommerce\Emails;
 defined( 'ABSPATH' ) || exit;
 
 use WC_Email;
+use WP_User;
 
 /**
  * Auction Watchers Live extend the custom WooCommerce email class
@@ -19,6 +20,27 @@ use WC_Email;
  * @extends WC_Email
  */
 class AuctionWatchersLive extends WC_Email {
+
+	/**
+	 * Site Name
+	 *
+	 * @var string
+	 */
+	public $site_name;
+
+	/**
+	 * User login name.
+	 *
+	 * @var string
+	 */
+	public $user_name;
+
+	/**
+	 * User ID.
+	 *
+	 * @var integer
+	 */
+	public $user_id;
 
 	/**
 	 * Set email defaults
@@ -39,13 +61,28 @@ class AuctionWatchersLive extends WC_Email {
 	}
 
 	/**
+	 * Get site name.
+	 *
+	 * @since  1.0.0
+	 * @return string
+	 */
+	public function get_site_name() {
+		return get_bloginfo( 'name' );
+	}
+
+
+	/**
 	 * Get email subject.
 	 *
 	 * @since  1.0.0
 	 * @return string
 	 */
 	public function get_default_subject() {
-		return __( 'subject', 'goodbids' );
+		return sprintf(
+			'%s %s is live',
+			esc_html( $this->get_site_name() ),
+			esc_html( $this->get_auction_title() )
+		);
 	}
 
 	/**
@@ -55,8 +92,77 @@ class AuctionWatchersLive extends WC_Email {
 	 * @return string
 	 */
 	public function get_default_heading() {
-		return __( 'Heading', 'goodbids' );
+		return __( 'Ready to GOODBID?', 'goodbids' );
 	}
+
+	/**
+	 * Get button text
+	 *
+	 * @since   1.0.0
+	 * @return string
+	 */
+	public function get_default_button_text() {
+		return __( 'Bid Now', 'goodbids' );
+	}
+
+	/**
+	 * Get the auction title
+	 *
+	 * @since   1.0.0
+	 * @return string
+	 */
+	public function get_auction_title() {
+		return __( '{auction_title}', 'goodbids' );
+	}
+
+	/**
+	 * Get the auction goal
+	 *
+	 * @since   1.0.0
+	 * @return string
+	 */
+	public function get_auction_goal() {
+		return __( '{auction_goal}', 'goodbids' );
+	}
+
+	/**
+	 * Get the auction url
+	 *
+	 * @since   1.0.0
+	 * @return string
+	 */
+	public function get_auction_url() {
+		return __( '{auction_url}', 'goodbids' );
+	}
+
+
+	/**
+	 * Get the auction starting bid
+	 *
+	 * @since   1.0.0
+	 * @return string
+	 */
+	public function get_auction_starting_bid() {
+		return __( '{auction_starting_bid}', 'goodbids' );
+	}
+
+	/**
+	 * Get user name.
+	 *
+	 * @since   1.0.0
+	 * @param mixed $id
+	 * @return string
+	 */
+	public function get_user_name() {
+		if ( ! $this->user_id ) {
+			return;
+		}
+
+		$user_first_name = get_user_meta( $this->user_id, 'first_name', true );
+
+		return $user_first_name ? $user_first_name : $this->object->user_login;
+	}
+
 
 	/**
 	 * Get the email recipients
@@ -83,16 +189,23 @@ class AuctionWatchersLive extends WC_Email {
 	 * Determine if the email should actually be sent and setup email merge variables
 	 *
 	 * @since 1.0.0
+	 * @param mixed $user_id
 	 * @return void
 	 */
-	public function trigger(): void {
+	public function trigger( $user_id ): void {
+		$this->setup_locale();
+
 		// TODO set up check before sending email
+		if ( $user_id ) {
+			$this->object    = new WP_User( $user_id );
+			$this->user_id   = $this->object->ID;
+			$this->user_name = stripslashes( $this->get_user_name() );
+		}
 
 		if ( ! $this->is_enabled() || ! $this->get_recipient() ) {
 			return;
 		}
 
-		$this->setup_locale();
 		// woohoo, send the email!
 		$this->send( $this->get_recipient(), $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments() );
 
@@ -109,7 +222,14 @@ class AuctionWatchersLive extends WC_Email {
 		return wc_get_template_html(
 			$this->template_html,
 			[
-				'email_heading' => $this->get_default_heading(),
+				'email_heading'        => $this->get_default_heading(),
+				'user_name'            => $this->get_user_name(),
+				'site_name'            => $this->get_site_name(),
+				'auction_title'        => $this->get_auction_title(),
+				'auction_goal'         => $this->get_auction_goal(),
+				'auction_url'          => $this->get_auction_url(),
+				'auction_starting_bid' => $this->get_auction_starting_bid(),
+				'button_text'          => $this->get_default_button_text(),
 			]
 		);
 	}
@@ -125,7 +245,14 @@ class AuctionWatchersLive extends WC_Email {
 		return wc_get_template_html(
 			$this->template_plain,
 			[
-				'email_heading' => $this->get_default_heading(),
+				'email_heading'        => $this->get_default_heading(),
+				'user_name'            => $this->get_user_name(),
+				'site_name'            => $this->get_site_name(),
+				'auction_title'        => $this->get_auction_title(),
+				'auction_goal'         => $this->get_auction_goal(),
+				'auction_url'          => $this->get_auction_url(),
+				'auction_starting_bid' => $this->get_auction_starting_bid(),
+				'button_text'          => $this->get_default_button_text(),
 			]
 		);
 	}
