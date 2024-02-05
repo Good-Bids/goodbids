faaec8073673096b0bbc5fc28287253e80a64076
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-02-05 16:52:04 -0700

    [#237] fixing token refactor

--- client-mu-plugins/goodbids/src/classes/Plugins/WooCommerce/Emails/AuctionOutbid.php
+++ client-mu-plugins/goodbids/src/classes/Plugins/WooCommerce/Emails/AuctionOutbid.php
@@ -22,20 +22,6 @@ use WP_User;
 class AuctionOutbid extends WC_Email {
 
 	/**
-	 * Site Name
-	 *
-	 * @var string
-	 */
-	public $site_name;
-
-	/**
-	 * User login name.
-	 *
-	 * @var string
-	 */
-	public $user_name;
-
-	/**
 	 * User ID.
 	 *
 	 * @var integer
@@ -61,31 +47,6 @@ class AuctionOutbid extends WC_Email {
 	}
 
 	/**
-	 * Get site name.
-	 *
-	 * @since  1.0.0
-	 * @return string
-	 */
-	public function get_site_name() {
-		return get_bloginfo( 'name' );
-	}
-
-
-	/**
-	 * Get email subject.
-	 *
-	 * @since  1.0.0
-	 * @return string
-	 */
-	public function get_default_subject() {
-		return sprintf(
-			'%s %s is live',
-			esc_html( $this->get_site_name() ),
-			esc_html( $this->get_auction_title() )
-		);
-	}
-
-	/**
 	 * Get email heading.
 	 *
 	 * @since   1.0.0
@@ -106,63 +67,17 @@ class AuctionOutbid extends WC_Email {
 	}
 
 	/**
-	 * Get the auction title
+	 * Get email subject.
 	 *
-	 * @since   1.0.0
+	 * @since  1.0.0
 	 * @return string
 	 */
-	public function get_auction_title() {
-		return __( '{auction_title}', 'goodbids' );
+	public function get_default_subject() {
+		return sprintf(
+			'%s You’ve been outbid',
+			'{site_title}',
+		);
 	}
-
-	/**
-	 * Get the auction goal
-	 *
-	 * @since   1.0.0
-	 * @return string
-	 */
-	public function get_auction_goal() {
-		return __( '{auction_goal}', 'goodbids' );
-	}
-
-	/**
-	 * Get the auction url
-	 *
-	 * @since   1.0.0
-	 * @return string
-	 */
-	public function get_auction_url() {
-		return __( '{auction_url}', 'goodbids' );
-	}
-
-
-	/**
-	 * Get the auction starting bid
-	 *
-	 * @since   1.0.0
-	 * @return string
-	 */
-	public function get_auction_starting_bid() {
-		return __( '{auction_starting_bid}', 'goodbids' );
-	}
-
-	/**
-	 * Get user name.
-	 *
-	 * @since   1.0.0
-	 * @param mixed $id
-	 * @return string
-	 */
-	public function get_user_name() {
-		if ( ! $this->user_id ) {
-			return;
-		}
-
-		$user_first_name = get_user_meta( $this->user_id, 'first_name', true );
-
-		return $user_first_name ? $user_first_name : $this->object->user_login;
-	}
-
 
 	/**
 	 * Get the email recipients
@@ -197,9 +112,8 @@ class AuctionOutbid extends WC_Email {
 
 		// TODO set up check before sending email
 		if ( $user_id ) {
-			$this->object    = new WP_User( $user_id );
-			$this->user_id   = $this->object->ID;
-			$this->user_name = stripslashes( $this->get_user_name() );
+			$this->object  = new WP_User( $user_id );
+			$this->user_id = $this->object->ID;
 		}
 
 		if ( ! $this->is_enabled() || ! $this->get_recipient() ) {
@@ -222,14 +136,8 @@ class AuctionOutbid extends WC_Email {
 		return wc_get_template_html(
 			$this->template_html,
 			[
-				'email_heading'        => $this->get_default_heading(),
-				'user_name'            => $this->get_user_name(),
-				'site_name'            => $this->get_site_name(),
-				'auction_title'        => $this->get_auction_title(),
-				'auction_goal'         => $this->get_auction_goal(),
-				'auction_url'          => $this->get_auction_url(),
-				'auction_starting_bid' => $this->get_auction_starting_bid(),
-				'button_text'          => $this->get_default_button_text(),
+				'email_heading' => $this->get_default_heading(),
+				'button_text'   => $this->get_default_button_text(),
 			]
 		);
 	}
@@ -245,14 +153,8 @@ class AuctionOutbid extends WC_Email {
 		return wc_get_template_html(
 			$this->template_plain,
 			[
-				'email_heading'        => $this->get_default_heading(),
-				'user_name'            => $this->get_user_name(),
-				'site_name'            => $this->get_site_name(),
-				'auction_title'        => $this->get_auction_title(),
-				'auction_goal'         => $this->get_auction_goal(),
-				'auction_url'          => $this->get_auction_url(),
-				'auction_starting_bid' => $this->get_auction_starting_bid(),
-				'button_text'          => $this->get_default_button_text(),
+				'email_heading' => $this->get_default_heading(),
+				'button_text'   => $this->get_default_button_text(),
 			]
 		);
 	}
