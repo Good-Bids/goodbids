9d5e7e90cd4b0ea7982d10b225fbbe7e4d0c6082
Brian DiChiara <brian.dichiara@viget.com>
2024-02-09 07:49:36 -0600

    [#377] Secure Log File Downloads

--- client-mu-plugins/goodbids/src/classes/Network/Logs.php
+++ client-mu-plugins/goodbids/src/classes/Network/Logs.php
@@ -8,6 +8,8 @@
 
 namespace GoodBids\Network;
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 use GoodBids\Utilities\Log;
 
 /**
@@ -26,9 +28,20 @@ class Logs {
 	const PAGE_SLUG = 'gb-logs';
 
 	/**
+	 * Download Log Query Var
+	 *
+	 * @since 1.0.0
+	 * @var string
+	 */
+	const DOWNLOAD_SLUG = 'gb-log';
+
+	/**
 	 * @since 1.0.0
 	 */
-	public function __construct() {}
+	public function __construct() {
+		// Download log files.
+		$this->maybe_download_log_file();
+	}
 
 	/**
 	 * Get an array of log files, sorted by date.
@@ -46,13 +59,15 @@ class Logs {
 
 		$files = scandir( $dir );
 
-		foreach ( $files as $file ) {
+		foreach ( $files as $file_name ) {
 			// Skip hidden files.
-			if ( str_starts_with( $file, '.' ) ) {
+			if ( str_starts_with( $file_name, '.' ) ) {
 				continue;
 			}
 
-			$logs[] = Log::get_logs_dir() . $file;
+			$hash = $this->encrypt_log_file( $file_name );
+
+			$logs[ $hash ] = Log::get_logs_dir() . $file_name;
 		}
 
 		return collect( $logs )
@@ -62,5 +77,105 @@ class Logs {
 				}
 			)
 			->all();
+	}
+
+	/**
+	 * Get encryption key for log file names.
+	 *
+	 * @since 1.0.0
+	 * @return string
+	 */
+	private function get_encryption_key(): string {
+		$env_var = goodbids()->get_config( 'vip-constants.auctioneer.api-key' );
+
+		if ( ! $env_var ) {
+			Log::error( 'Missing Auctioneer API Key constants config.' );
+			return SECURE_AUTH_SALT;
+		}
+
+		return vip_get_env_var( $env_var, null );
+	}
+
+	/**
+	 * Encrypt the log file name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $file_name
+	 *
+	 * @return string
+	 */
+	private function encrypt_log_file( string $file_name ): string {
+		$key = $this->get_encryption_key();
+
+		if ( ! $key ) {
+			Log::error( 'Missing Encryption Key.' );
+			return $file_name;
+		}
+
+		return JWT::encode( compact( 'file_name' ), $key, 'HS256' );
+	}
+
+	/**
+	 * Decrypt the log file name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $encrypted
+	 *
+	 * @return string
+	 */
+	private function decrypt_log_file( string $encrypted ): string {
+		$key = $this->get_encryption_key();
+
+		if ( ! $key ) {
+			Log::error( 'Missing Encryption Key.' );
+			return $encrypted;
+		}
+
+		$decrypted = JWT::decode( $encrypted, new Key( $key, 'HS256' ) );
+
+		return $decrypted->file_name;
+	}
+
+	/**
+	 * Stream the log file when download is requested.
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
+	private function maybe_download_log_file(): void {
+		add_action(
+			'admin_init',
+			function() {
+				global $pagenow;
+
+				if ( ! is_network_admin() || 'admin.php' !== $pagenow ) {
+					return;
+				}
+
+				if ( empty( $_GET['page'] ) || sanitize_text_field( $_GET['page'] ) !== self::PAGE_SLUG || empty( $_GET[ self::DOWNLOAD_SLUG ] ) ) { // phpcs:ignore
+					return;
+				}
+
+				$encrypted = sanitize_text_field( $_GET[ self::DOWNLOAD_SLUG ] ); // phpcs:ignore
+				$file_name = $this->decrypt_log_file( $encrypted );
+				$file_path = Log::get_logs_dir() . $file_name;
+
+				if ( ! file_exists( $file_path ) ) {
+					wp_die( 'Error: File not found.' );
+				}
+
+				header( 'Content-Description: File Transfer' );
+				header( 'Content-Type: application/octet-stream' );
+				header( 'Content-Disposition: attachment; filename="' . basename( $file_path ) . '"' );
+				header( 'Expires: 0' );
+				header( 'Cache-Control: must-revalidate' );
+				header( 'Pragma: public' );
+				header( 'Content-Length: ' . filesize( $file_path ) );
+				readfile( $file_path );
+				exit;
+			}
+		);
 	}
 }
--- client-mu-plugins/goodbids/views/network/logs.php
+++ client-mu-plugins/goodbids/views/network/logs.php
@@ -6,17 +6,19 @@
  * @package GoodBids
  */
 
+use GoodBids\Network\Logs;
+
 $log_files = goodbids()->network->logs->get_log_files();
 ?>
 <div class="wrap">
 	<h2><?php esc_html_e( 'Logs', 'goodbids' ); ?></h2>
 
 	<ul>
-		<?php foreach ( $log_files as $log_file ) : ?>
+		<?php foreach ( $log_files as $key => $log_file ) : ?>
 			<li>
 				<?php echo esc_html( basename( $log_file ) ); ?>
 
-				<a href="<?php echo esc_url( $log_file ); ?>" download><?php esc_html_e( 'Download', 'goodbids' ); ?></a>
+				<a href="<?php echo esc_url( add_query_arg( Logs::DOWNLOAD_SLUG, $key ) ); ?>" download><?php esc_html_e( 'Download', 'goodbids' ); ?></a>
 			</li>
 		<?php endforeach; ?>
 	</ul>
