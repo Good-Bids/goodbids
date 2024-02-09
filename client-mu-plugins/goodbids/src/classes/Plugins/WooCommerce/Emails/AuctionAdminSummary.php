627cf4b4754c9d2bee04297e345a944c0f5d599b
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-02-09 12:26:20 -0700

    [#248] Auction Summary (#379)
    * [#248] setting up the base email
    
    * [#248] setting up summery template

---
+++ client-mu-plugins/goodbids/src/classes/Plugins/WooCommerce/Emails/AuctionAdminSummary.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * Auction Watchers Live: Send an email to the users that are watching when an auction goes live.
+ *
+ * @since 1.0.0
+ * @package GoodBids
+ */
+
+namespace GoodBids\Plugins\WooCommerce\Emails;
+
+defined( 'ABSPATH' ) || exit;
+
+use WC_Email;
+use WP_User;
+
+/**
+ * Auction Watchers Live extend the custom WooCommerce email class
+ *
+ * @since 1.0.0
+ * @extends WC_Email
+ */
+class AuctionAdminSummary extends WC_Email {
+
+	/**
+	 * User ID.
+	 *
+	 * @var integer
+	 */
+	public $user_id;
+
+	/**
+	 * Set email defaults
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		$this->id             = 'goodbids_auction_admin_summary';
+		$this->title          = __( 'Auction Admin Summary', 'goodbids' );
+		$this->description    = __( 'Notification email sent to admins when an auction closes.', 'goodbids' );
+		$this->template_html  = 'emails/auction-admin-summery.php';
+		$this->template_plain = 'emails/plain/auction-admin-summery.php';
+
+		// TODO: Trigger this email.
+
+		// Call parent constructor to load any other defaults not explicitly defined here
+		parent::__construct();
+	}
+
+
+	/**
+	 * Get email subject.
+	 *
+	 * @since  1.0.0
+	 * @return string
+	 */
+	public function get_default_subject() {
+		return sprintf(
+			'%s Auction summary for %s',
+			'{site_title}',
+			'{auction.title}'
+		);
+	}
+
+	/**
+	 * Get email heading.
+	 *
+	 * @since   1.0.0
+	 * @return string
+	 */
+	public function get_default_heading() {
+		return sprintf(
+			'%s raised for %s!',
+			'{auction.totalRaised}',
+			'{site_title}'
+		);
+	}
+
+	/**
+	 * Get the email recipients
+	 *
+	 * @return string
+	 */
+	public function get_recipient(): string {
+		$recipient = parent::get_recipient();
+
+		if ( ! $recipient ) {
+			// this sets the recipient to the settings defined below in init_form_fields()
+			$recipient = $this->get_option( 'recipient' );
+		}
+
+		// if none was entered, just use the WP admin email as a fallback
+		if ( ! $recipient ) {
+			$recipient = get_option( 'admin_email' );
+		}
+
+		return $recipient;
+	}
+
+	/**
+	 * Determine if the email should actually be sent and setup email merge variables
+	 *
+	 * @since 1.0.0
+	 * @param mixed $user_id
+	 * @return void
+	 */
+	public function trigger( $user_id ): void {
+		$this->setup_locale();
+
+		// TODO set up check before sending email
+		if ( $user_id ) {
+			$this->object  = new WP_User( $user_id );
+			$this->user_id = $this->object->ID;
+		}
+
+		if ( ! $this->is_enabled() || ! $this->get_recipient() ) {
+			return;
+		}
+
+		// woohoo, send the email!
+		$this->send( $this->get_recipient(), $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments() );
+
+		$this->restore_locale();
+	}
+
+	/**
+	 * get_content_html function.
+	 *
+	 * @since 1.0.0
+	 * @return string
+	 */
+	public function get_content_html(): string {
+		return wc_get_template_html(
+			$this->template_html,
+			[
+				'instance'      => $this,
+				'email_heading' => $this->get_default_heading(),
+			]
+		);
+	}
+
+
+	/**
+	 * get_content_plain function.
+	 *
+	 * @since 1.0.0
+	 * @return string
+	 */
+	public function get_content_plain(): string {
+		return wc_get_template_html(
+			$this->template_plain,
+			[
+				'instance'      => $this,
+				'email_heading' => $this->get_default_heading(),
+			]
+		);
+	}
+
+	/**
+	 * Initialize Settings Form Fields
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
+	public function init_form_fields(): void {
+
+		$this->form_fields = [
+			'enabled'            => [
+				'title'   => __( 'Enable/Disable', 'goodbids' ),
+				'type'    => 'checkbox',
+				'label'   => __( 'Enable this email notification', 'goodbids' ),
+				'default' => 'yes',
+			],
+			'subject'            => [
+				'title'       => __( 'Subject', 'goodbids' ),
+				'type'        => 'text',
+				'desc_tip'    => true,
+				'description' => sprintf( 'This controls the email subject line. Leave blank to use the default subject: <code>%s</code>.', $this->get_subject() ),
+				'placeholder' => $this->get_default_subject(),
+				'default'     => '',
+			],
+			'heading'            => [
+				'title'       => __( 'Email Heading', 'goodbids' ),
+				'type'        => 'text',
+				'desc_tip'    => true,
+				'description' => sprintf( __( 'This controls the main heading contained within the email notification. Leave blank to use the default heading: <code>%s</code>.' ), $this->get_heading() ),
+				'placeholder' => $this->get_default_heading(),
+				'default'     => '',
+			],
+			'additional_content' => [
+				'title'       => __( 'Additional content', 'goodbids' ),
+				'description' => __( 'Text to appear below the main email content.', 'goodbids' ),
+				'css'         => 'width:400px; height: 75px;',
+				'placeholder' => __( 'N/A', 'goodbids' ),
+				'type'        => 'textarea',
+				'default'     => $this->get_default_additional_content(),
+				'desc_tip'    => true,
+			],
+			'email_type'         => [
+				'title'       => __( 'Email type', 'goodbids' ),
+				'type'        => 'select',
+				'description' => __( 'Choose which format of email to send.', 'goodbids' ),
+				'default'     => 'html',
+				'class'       => 'email_type wc-enhanced-select',
+				'options'     => $this->get_email_type_options(),
+				'desc_tip'    => true,
+			],
+		];
+	}
+}
