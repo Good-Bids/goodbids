b7b9c6cd24583dc74e59fb20a0e7f57bf141ff56
Nick Telsan <nick.telsan@viget.com>
2024-02-12 11:32:57 -0500

    [#278] Free Bid Button
    - Adjusts render of free bid button

---
+++ client-mu-plugins/goodbids/src/blocks/bidding/components/free-bid-button/index.tsx
@@ -0,0 +1,43 @@
+import clsx from 'clsx';
+import { useBiddingState } from '../../store';
+import { AnimatePresence, motion } from 'framer-motion';
+
+export function FreeBidButton() {
+	const {
+		userFreeBids,
+		auctionStatus,
+		bidUrl,
+		freeBidsAllowed,
+		isLastBidder,
+		userId,
+	} = useBiddingState();
+
+	const disabled = isLastBidder || userFreeBids < 1;
+
+	const freeBidUrl = `${bidUrl}&use-free-bid=1`;
+
+	const classes = clsx('btn-fill-secondary text-center', {
+		'pointer-events-none cursor-not-allowed !text-contrast-4': disabled,
+	});
+
+	return (
+		<AnimatePresence>
+			{freeBidsAllowed && auctionStatus === 'live' && (
+				<motion.a
+					layout
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					transition={{ duration: 0.2 }}
+					href={disabled ? '' : freeBidUrl}
+					className={classes}
+					aria-disabled={disabled}
+				>
+					{`Place free bid ${
+						userId ? `(${userFreeBids} available)` : ''
+					}`}
+				</motion.a>
+			)}
+		</AnimatePresence>
+	);
+}
