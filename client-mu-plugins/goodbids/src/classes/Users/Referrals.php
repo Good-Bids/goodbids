d8b4edbc40f2aac1a5df462f7476a0d322d182d0
Brian DiChiara <brian.dichiara@viget.com>
2024-02-14 10:42:04 -0600

    [#392] WIP: Migrating plugin code to GoodBids

---
+++ client-mu-plugins/goodbids/src/classes/Users/Referrals.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Referrals Functionality
+ *
+ * @since 1.0.0
+ * @package GoodBids
+ */
+
+namespace GoodBids\Users;
+
+/**
+ * Class for User Referrals
+ *
+ * @since 1.0.0
+ */
+class Referrals {
+
+	/**
+	 * Initialize User Referrals
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+
+	}
+
+}
