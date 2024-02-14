4144f3397421021428131604b78599e76137f81c
Brian DiChiara <brian.dichiara@viget.com>
2024-02-14 06:54:33 -0600

    [N/A] PR Feedback

--- client-mu-plugins/goodbids/src/helpers.php
+++ client-mu-plugins/goodbids/src/helpers.php
@@ -22,3 +22,30 @@ if ( ! function_exists( 'block_attr' ) ) {
 		goodbids()->acf->blocks()->block_attr( $block, $addl_class, $attr );
 	}
 }
+
+if ( ! function_exists( 'dd' ) ) {
+	/**
+	 * Helpful Dump & Die method for printing debug data to the screen
+	 *
+	 * @link https://gist.github.com/james2doyle/abfbd4dc5754712bac022faf4e2881a6
+	 *
+	 * @param mixed $data
+	 *
+	 * @return void
+	 */
+	function dd( mixed $data ): void {
+		// Disable if not in dev environment.
+		if ( ! GoodBids\Core::is_dev_env() ) {
+			return;
+		}
+
+		ini_set( 'highlight.comment', '#969896; font-style: italic' );
+		ini_set( 'highlight.default', '#FFFFFF' );
+		ini_set( 'highlight.html', '#D16568' );
+		ini_set( 'highlight.keyword', '#7FA3BC; font-weight: bold' );
+		ini_set( 'highlight.string', '#F2C47E' );
+
+		$output = highlight_string( "<?php\n\n" . var_export( $data, true ), true );
+		die( "<div style=\"background-color: #1C1E21; padding: 1rem\">{$output}</div>" );
+	}
+}
--- client-mu-plugins/vip-env-vars.example.php
+++ client-mu-plugins/vip-env-vars.example.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Local environment variables
+ */
 
 /**
  * Copy and paste this file, renaming it to "vip-env-vars.local.php"
@@ -11,10 +14,24 @@
  * passwords and api keys should be left as TODOs
  */
 
-//
+/**
+ * Auctioneer Environment Variables
+ */
 defined( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_URL_LOCAL' ) || define( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_URL_LOCAL', 'http://localhost:3000' );
 defined( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_URL_DEVELOP' ) || define( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_URL_DEVELOP', 'https://goodbids-node-develop.go-vip.net' );
 defined( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_URL_PRODUCTION' ) || define( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_URL_PRODUCTION', 'https://goodbids-node.go-vip.net' );
 
-// TODO
-// defined( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_API_KEY' ) || define( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_API_KEY', 'YOUR_VALUE_HERE' );
+/**
+ * Current Development Environment can be obtained by using:
+ * `vip config envvar get API_KEY -a goodbids -e develop`
+ *
+ * TODO
+ */
+defined( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_API_KEY' ) || define( 'VIP_ENV_VAR_GOODBIDS_AUCTIONEER_API_KEY', 'YOUR_VALUE_HERE' );
+
+/**
+ * ACF Environment Variables
+ *
+ * TODO
+ */
+defined( 'VIP_ENV_VAR_GOODBIDS_ACF_LICENSE_KEY' ) || define( 'VIP_ENV_VAR_GOODBIDS_ACF_LICENSE_KEY', 'YOUR_VALUE_HERE' );
