7bcd8db2ca8fb9541c92f28a1db89ea0668f3c2d
Nick Telsan <nick.telsan@viget.com>
2024-02-16 13:14:41 -0500

    Commentos

---
+++ client-mu-plugins/goodbids/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const defaults = require('@wordpress/scripts/config/webpack.config.js');
+
+module.exports = {
+	...defaults,
+	entry: {
+		biddingView: {
+			import: path.resolve(
+				process.cwd(),
+				'src/blocks/bidding',
+				'view.tsx',
+			),
+			filename: 'blocks/bidding/view.js',
+		},
+		biddingIndex: {
+			import: path.resolve(
+				process.cwd(),
+				'src/blocks/bidding',
+				'index.tsx',
+			),
+			filename: 'blocks/bidding/index.js',
+		},
+		admin: {
+			import: path.resolve(process.cwd(), 'src', 'admin.tsx'),
+			filename: 'admin.js',
+		},
+		editor: {
+			import: path.resolve(process.cwd(), 'src', 'editor.tsx'),
+			filename: 'editor.js',
+		},
+		main: {
+			import: path.resolve(process.cwd(), 'src', 'main.tsx'),
+			filename: 'main.js',
+		},
+		auctionWizardStart: {
+			import: path.resolve(
+				process.cwd(),
+				'src/views/auction-wizard/start',
+				'index.tsx',
+			),
+			filename: 'views/auction-wizard/start.js',
+		},
+		auctionWizardProduct: {
+			import: path.resolve(
+				process.cwd(),
+				'src/views/auction-wizard/product',
+				'index.tsx',
+			),
+			filename: 'views/auction-wizard/product.js',
+		},
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(process.cwd(), 'build'),
+	},
+};
