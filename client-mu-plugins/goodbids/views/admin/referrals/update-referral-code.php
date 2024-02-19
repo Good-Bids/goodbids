9fc633a67c8622d880b4503a46c1212a39d70ee1
Brian DiChiara <brian.dichiara@viget.com>
2024-02-19 11:25:34 -0600

    [#392] Another Screen Reader Text label

--- client-mu-plugins/goodbids/views/admin/referrals/update-referral-code.php
+++ client-mu-plugins/goodbids/views/admin/referrals/update-referral-code.php
@@ -16,14 +16,21 @@ use GoodBids\Users\Referrals\Referrer;
 <table class="form-table">
 	<?php wp_nonce_field( Admin::UPDATE_CODE_NONCE_ACTION, Admin::UPDATE_CODE_NONCE ); ?>
 	<tr>
-		<th><?php esc_html_e( 'Referral Code', 'goodbids' ); ?></th>
+		<th>
+			<label for="<?php echo esc_attr( Referrals::REFERRAL_CODE_META_KEY ); ?>">
+				<?php esc_html_e( 'Referral Code', 'goodbids' ); ?>
+			</label>
+		</th>
 
 		<td>
 			<label>
-				<input type="text" name="<?php echo esc_attr( Referrals::REFERRAL_CODE_META_KEY ); ?>"
+				<input
+					type="text"
+					name="<?php echo esc_attr( Referrals::REFERRAL_CODE_META_KEY ); ?>"
+					id="<?php echo esc_attr( Referrals::REFERRAL_CODE_META_KEY ); ?>"
 					placeholder="<?php esc_attr_e( 'Referral Code', 'goodbids' ); ?>"
 					value="<?php echo esc_attr( $referrer->get_code() ); ?>"
-					style="text-transform: uppercase"
+					style="text-transform: uppercase;"
 				/>
 			</label>
 		</td>
