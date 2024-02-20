45a6ad4a6709c87daf277bf9ef086355af1ef3ef
Brian DiChiara <brian.dichiara@viget.com>
2024-02-20 16:47:20 -0600

    [#365] Adjustments to Auction Closed Email

--- client-mu-plugins/goodbids/views/woocommerce/emails/auction-closed.php
+++ client-mu-plugins/goodbids/views/woocommerce/emails/auction-closed.php
@@ -25,7 +25,7 @@ do_action( 'woocommerce_email_header', $email_heading );
 	<?php
 	printf(
 		/* translators: %1$s: Auction title, %2$s: Auction total Bids, %3$s: Auction total Raised, %4$s: Site title, %5$s: user Total Bids, %6$s: User Total Donated */
-		esc_html__( 'The %1$s auction has ended with %2$s placed and %3$s raised for %4$s. You supported this auction with %5$s for a total donation of %6$s', 'goodbids' ),
+		esc_html__( 'The %1$s auction has ended with %2$s bid(s) placed and %3$s raised for %4$s. You supported this auction with %5$s bid(s) for a total donation of %6$s.', 'goodbids' ),
 		'{auction.title}',
 		'{auction.bid_count}',
 		'{auction.total_raised}',
@@ -37,7 +37,15 @@ do_action( 'woocommerce_email_header', $email_heading );
 </p>
 
 <p>
-	<?php esc_html_e( 'Check your email or visit the auction page to see if you won!', 'goodbids' ); ?>
+	<?php
+	printf(
+		'%s <a href="%s">%s</a> %s!',
+		esc_html__( 'Check your email or', 'goodbids' ),
+		'{auction.url}',
+		esc_html__( 'visit the auction page', 'goodbids' ),
+		esc_html__( 'to see if you won!', 'goodbids' )
+	);
+	?>
 </p>
 
 <?php
