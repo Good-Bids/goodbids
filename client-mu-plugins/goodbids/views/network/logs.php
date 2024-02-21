f9c8e1a249f8644869f01b99a70e43aeaabc5285
Brian DiChiara <brian.dichiara@viget.com>
2024-02-21 16:44:18 -0600

    [N/A] Display Link to New Relic if non-local.

--- client-mu-plugins/goodbids/views/network/logs.php
+++ client-mu-plugins/goodbids/views/network/logs.php
@@ -6,62 +6,73 @@
  * @package GoodBids
  */
 
+use GoodBids\Core;
 use GoodBids\Network\Logs;
 ?>
 <div class="wrap <?php echo esc_attr( Logs::PAGE_SLUG ); ?>">
 	<h2><?php esc_html_e( 'Logs', 'goodbids' ); ?></h2>
 
-	<?php goodbids()->sites->loop(
-		function () {
-			$log_files = goodbids()->network->logs->get_log_files();
+	<?php if ( Core::is_dev_env() ): ?>
 
-			if ( empty( $log_files ) ) {
-				return;
-			}
+		<?php goodbids()->sites->loop(
+			function () {
+				$log_files = goodbids()->network->logs->get_log_files();
 
-			$total_pages = goodbids()->network->logs->get_total_pages();
+				if ( empty( $log_files ) ) {
+					return;
+				}
 
-			printf(
-				'<h3>%s</h3>',
-				esc_html( get_bloginfo( 'name' ) )
-			);
+				$total_pages = goodbids()->network->logs->get_total_pages();
 
-			?>
-			<ul class="gb-log-files">
-				<?php foreach ( $log_files as $key => $log_file ) : ?>
-					<li>
-						<a href="<?php echo esc_url( add_query_arg( Logs::DOWNLOAD_SLUG, $key ) ); ?>" download><?php esc_html_e( 'Download', 'goodbids' ); ?></a>
-
-						<span class="file-name"><?php echo esc_html( basename( $log_file ) ); ?></span>
-
-						<span class="file-size">(<?php echo esc_html( size_format( filesize( $log_file ) ) ); ?>)</span>
-					</li>
-				<?php endforeach; ?>
-			</ul>
-
-			<?php
-			if ( $total_pages > 1 ) :
-				$page_url     = remove_query_arg( Logs::PAGINATION_PARAM );
-				$current_page = goodbids()->network->logs->get_current_page();
-
-				echo wp_kses_post(
-					paginate_links(
-						[
-							'base'      => esc_url_raw( $page_url . '%_%' ),
-							'format'    => '&' . Logs::PAGINATION_PARAM . '=%#%',
-							'add_args'  => false,
-							'current'   => $current_page,
-							'total'     => $total_pages,
-							'prev_text' => '&larr;',
-							'next_text' => '&rarr;',
-							'type'      => 'list',
-							'end_size'  => 3,
-							'mid_size'  => 3,
-						]
-					)
+				printf(
+					'<h3>%s</h3>',
+					esc_html( get_bloginfo( 'name' ) )
 				);
-			endif;
-		}
-	);
-	?>
+
+				?>
+				<ul class="gb-log-files">
+					<?php foreach ( $log_files as $key => $log_file ) : ?>
+						<li>
+							<a href="<?php echo esc_url( add_query_arg( Logs::DOWNLOAD_SLUG, $key ) ); ?>" download><?php esc_html_e( 'Download', 'goodbids' ); ?></a>
+
+							<span class="file-name"><?php echo esc_html( basename( $log_file ) ); ?></span>
+
+							<span class="file-size">(<?php echo esc_html( size_format( filesize( $log_file ) ) ); ?>)</span>
+						</li>
+					<?php endforeach; ?>
+				</ul>
+
+				<?php
+				if ( $total_pages > 1 ) :
+					$page_url     = remove_query_arg( Logs::PAGINATION_PARAM );
+					$current_page = goodbids()->network->logs->get_current_page();
+
+					echo wp_kses_post(
+						paginate_links(
+							[
+								'base'      => esc_url_raw( $page_url . '%_%' ),
+								'format'    => '&' . Logs::PAGINATION_PARAM . '=%#%',
+								'add_args'  => false,
+								'current'   => $current_page,
+								'total'     => $total_pages,
+								'prev_text' => '&larr;',
+								'next_text' => '&rarr;',
+								'type'      => 'list',
+								'end_size'  => 3,
+								'mid_size'  => 3,
+							]
+						)
+					);
+				endif;
+			}
+		);
+		?>
+	<?php else : ?>
+		<?php printf(
+			'<p>%s <a href="%s" target="_blank" rel="noopener">%s</a>.</p>',
+			esc_html__( 'Logs can be found in', 'goodbids' ),
+			'https://one.newrelic.com/nr1-core?account=4344280&state=d9de82a8-b76e-444a-0e0b-77e4b28b0f7f',
+			esc_html__( 'NewRelic', 'goodbids' )
+		); ?>
+	<?php endif; ?>
 </div>
