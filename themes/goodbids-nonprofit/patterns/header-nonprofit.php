8f29fb0dc60e0346991e361e6d9fc08bac9c5aaa
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-02-21 12:12:46 -0700

    [#429] updating header logo style

--- themes/goodbids-nonprofit/patterns/header-nonprofit.php
+++ themes/goodbids-nonprofit/patterns/header-nonprofit.php
@@ -1,18 +1,13 @@
 <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"20px","bottom":"20px"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide has-background"
-	style="padding-top:20px;padding-bottom:20px">
+<div class="wp-block-group alignwide has-background" style="padding-top:20px;padding-bottom:20px">
 	<!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} -->
 	<div class="wp-block-group alignwide">
 		<!-- wp:group {"style":{"spacing":{"blockGap":"0.2rem"},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex"}} -->
 		<div class="wp-block-group">
 
-			<!-- wp:site-logo {"width":160,"shouldSyncIcon":true,"style":{"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0.5rem"}}}} /-->
-
-			<!-- wp:group {"style":{"spacing":{"blockGap":"0px"}}} -->
-			<div class="wp-block-group">
-				<!-- wp:paragraph {"style":{"layout":{"selfStretch":"fixed","flexSize":"25px"}},"fontSize":"x-small"} -->
-					<p class="has-x-small-font-size"><?php esc_html_e( 'for', 'goodbids-nonprofit' ); ?></p>
-				<!-- /wp:paragraph -->
+			<!-- wp:group {"style":{"border":{"radius":"0px"}},"layout":{"type":"constrained"}} -->
+			<div class="overflow-hidden rounded-xs wp-block-group">
+				<!-- wp:site-logo {"width":48,"shouldSyncIcon":false,"className":"is-style-default","style":{"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"1rem"}}}} /-->
 			</div>
 			<!-- /wp:group -->
 
@@ -27,7 +22,8 @@
 		<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
 		<div class="wp-block-group">
 			<!-- wp:navigation {"layout":{"type":"flex","justifyContent":"right","orientation":"horizontal"},"style":{"spacing":{"margin":{"top":"0"},"blockGap":"var:preset|spacing|20"},"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"x-small"} /-->
-			<!-- wp:woocommerce/customer-account {"displayStyle":"text_only","iconStyle":"alt","iconClass":"wc-block-customer-account__account-icon","className":"btn","textColor":"contrast","fontSize":"x-small","style":{"layout":{"selfStretch":"fit","flexSize":null},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}}} /-->		</div>
+			<!-- wp:woocommerce/customer-account {"displayStyle":"text_only","iconStyle":"alt","iconClass":"wc-block-customer-account__account-icon","className":"btn","textColor":"contrast","fontSize":"x-small","style":{"layout":{"selfStretch":"fit","flexSize":null},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}}} /-->
+		</div>
 		<!-- /wp:group -->
 	</div>
 
