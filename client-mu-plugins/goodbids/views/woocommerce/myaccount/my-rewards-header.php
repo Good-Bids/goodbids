bf193978f96ff30a554e8cda6a0e89dfb8fac30d
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-02-26 13:44:16 -0700

    [#470] styling my account pages

--- client-mu-plugins/goodbids/views/woocommerce/myaccount/my-rewards-header.php
+++ client-mu-plugins/goodbids/views/woocommerce/myaccount/my-rewards-header.php
@@ -11,20 +11,20 @@ $claimed   = count( goodbids()->sites->get_user_reward_orders() );
 $unclaimed = $earned - $claimed;
 ?>
 <div class="mb-6 goodbids-rewards-header">
-	<ul class="flex flex-wrap gap-6 p-0 m-0 list-none goodbids-order-metrics">
-		<li class="w-full p-4 text-center rounded-sm sm:w-auto bg-contrast-5">
+	<ul class="flex flex-wrap gap-6 p-0 m-0 list-none md:grid md:grid-cols-3 goodbids-order-metrics">
+		<li class="w-full p-4 rounded-sm sm:w-auto bg-contrast-5">
 			<p class="m-0 font-thin uppercase has-x-small-font-size"><?php esc_html_e( 'Earned', 'goodbids' ); ?></p>
-			<p class="mt-2 mb-0 font-semibold"><?php echo esc_html( $earned ); ?></p>
+			<p class="mt-2 mb-0 text-lg font-bold"><?php echo esc_html( $earned ); ?></p>
 		</li>
 
-		<li class="w-full p-4 text-center rounded-sm sm:w-auto bg-contrast-5">
+		<li class="w-full p-4 rounded-sm sm:w-auto bg-contrast-5">
 			<p class="m-0 font-thin uppercase has-x-small-font-size"><?php esc_html_e( 'Claimed', 'goodbids' ); ?></p>
-			<p class="mt-2 mb-0 font-semibold"><?php echo esc_html( $claimed ); ?></p>
+			<p class="mt-2 mb-0 text-lg font-bold"><?php echo esc_html( $claimed ); ?></p>
 		</li>
 
-		<li class="w-full p-4 text-center rounded-sm sm:w-auto bg-contrast-5">
+		<li class="w-full p-4 rounded-sm sm:w-auto bg-contrast-5">
 			<p class="m-0 font-thin uppercase has-x-small-font-size"><?php esc_html_e( 'Unclaimed', 'goodbids' ); ?></p>
-			<p class="mt-2 mb-0 font-semibold"><?php echo esc_html( $unclaimed ); ?></p>
+			<p class="mt-2 mb-0 text-lg font-bold"><?php echo esc_html( $unclaimed ); ?></p>
 		</li>
 	</ul>
 </div>
