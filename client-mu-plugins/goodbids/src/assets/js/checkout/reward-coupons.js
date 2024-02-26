1a9fc0f670e05f603a5449f4371b8f3543517add
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-02-26 11:39:44 -0700

    [n/a] checking  blockscheckout in window.wc

--- client-mu-plugins/goodbids/src/assets/js/checkout/reward-coupons.js
+++ client-mu-plugins/goodbids/src/assets/js/checkout/reward-coupons.js
@@ -1,19 +1,19 @@
-const { registerCheckoutFilters } = window.wc.blocksCheckout;
+if ('wc' in window) {
+	const { registerCheckoutFilters } = window.wc.blocksCheckout;
 
-const rewardCoupons = (coupons) => {
-	return coupons.map((coupon) => {
-		if (!coupon.label.startsWith('gb_reward_')) {
-			return coupon;
-		}
+	const rewardCoupons = (coupons) => {
+		return coupons.map((coupon) => {
+			if (!coupon.label.startsWith('gb_reward_')) {
+				return coupon;
+			}
 
-		return {
-			...coupon,
-			label: 'Auction Reward',
-		};
-	});
-};
+			return {
+				...coupon,
+				label: 'Auction Reward',
+			};
+		});
+	};
 
-if (registerCheckoutFilters) {
 	registerCheckoutFilters('goodbids/reward-coupons', {
 		coupons: rewardCoupons,
 	});
