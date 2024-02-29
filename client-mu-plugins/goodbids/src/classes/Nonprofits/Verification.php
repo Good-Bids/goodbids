08412e67deeae7f6f44c8e1a9b2285e14117c2cd
Brian DiChiara <122309362+bd-viget@users.noreply.github.com>
2024-02-29 12:44:47 -0600

    [#519] Add Additional Nonprofit Fields (#538)
    * [#519] Add additional Nonprofit Fields
    
    * [#519] I18n on Select Values

--- client-mu-plugins/goodbids/src/classes/Nonprofits/Verification.php
+++ client-mu-plugins/goodbids/src/classes/Nonprofits/Verification.php
@@ -265,15 +265,24 @@ class Verification {
 					return $fields;
 				}
 
-				$fields['verification'] = [
-					'label'       => __( 'Verified', 'goodbids' ),
-					'type'        => 'toggle',
-					'default'     => '',
-					'placeholder' => '',
-					'description' => __( 'Only visible to Super Admins', 'goodbids' ),
-				];
+				$new_fields = [];
 
-				return $fields;
+				foreach ( $fields as $key => $field ) {
+					$new_fields[ $key ] = $field;
+
+					// Insert after Status.
+					if ( 'status' === $key ) {
+						$new_fields['verification'] = [
+							'label'       => __( 'Verified', 'goodbids' ),
+							'type'        => 'toggle',
+							'default'     => '',
+							'placeholder' => '',
+							'description' => __( 'Only visible to Super Admins', 'goodbids' ),
+						];
+					}
+				}
+
+				return $new_fields;
 			}
 		);
 	}
@@ -287,28 +296,28 @@ class Verification {
 	 */
 	private function init_custom_fields(): void {
 		$this->custom_fields = [
-			'legal-name' => [
+			'legal-name'          => [
 				'label'       => __( 'Nonprofit Legal Name', 'goodbids' ),
 				'type'        => 'text',
 				'default'     => '',
 				'placeholder' => '',
 				'required'    => true,
 			],
-			'ein'        => [
+			'ein'                 => [
 				'label'       => __( 'Nonprofit EIN', 'goodbids' ),
 				'type'        => 'text',
 				'default'     => '',
 				'placeholder' => 'XX-XXXXXXX',
 				'required'    => true,
 			],
-			'website'    => [
+			'website'              => [
 				'label'       => __( 'Nonprofit Website', 'goodbids' ),
 				'type'        => 'url',
 				'default'     => '',
 				'placeholder' => 'https://',
 				'required'    => true,
 			],
-			'status'     => [
+			'status'               => [
 				'label'       => __( 'Site Status', 'goodbids' ),
 				'type'        => 'select',
 				'default'     => 'pending',
@@ -329,6 +338,79 @@ class Verification {
 						'value' => 'disabled',
 					],
 				],
+			],
+			'sep1'                  => [
+				'type' => 'separator',
+			],
+			'primary_contact_name'  => [
+				'label'       => __( 'Primary Contact Legal Name', 'goodbids' ),
+				'type'        => 'text',
+				'default'     => '',
+				'placeholder' => '',
+				'required'    => true,
+			],
+			'primary_contact_email' => [
+				'label'       => __( 'Primary Contact Email Address', 'goodbids' ),
+				'type'        => 'email',
+				'default'     => '',
+				'placeholder' => 'email@domain.com',
+				'required'    => true,
+			],
+			'primary_contact_title' => [
+				'label'       => __( 'Primary Contact Job Title', 'goodbids' ),
+				'type'        => 'text',
+				'default'     => '',
+				'placeholder' => '',
+				'required'    => true,
+			],
+			'impact_vertical'       => [
+				'label'       => __( 'Impact Vertical', 'goodbids' ),
+				'type'        => 'select',
+				'default'     => '',
+				'placeholder' => '',
+				'required'    => true,
+				'options'     => [
+					__( 'Animals', 'goodbids' ),
+					__( 'Arts & Culture', 'goodbids' ),
+					__( 'Civic & Community', 'goodbids' ),
+					__( 'Disaster & Emergency Services', 'goodbids' ),
+					__( 'Diversity, Equity & Inclusion', 'goodbids' ),
+					__( 'Education/School', 'goodbids' ),
+					__( 'Environment & Sustainability', 'goodbids' ),
+					__( 'Faith-based & Religious', 'goodbids' ),
+					__( 'Food Insecurity', 'goodbids' ),
+					__( 'Health and Wellness', 'goodbids' ),
+					__( 'Housing & Homelessness', 'goodbids' ),
+					__( 'Humanities/International Affairs', 'goodbids' ),
+					__( 'Justice & Legal Services', 'goodbids' ),
+					__( 'Reproductive Rights', 'goodbids' ),
+					__( 'Research', 'goodbids' ),
+					__( 'Sports and Recreation', 'goodbids' ),
+					__( 'Technology', 'goodbids' ),
+					__( 'Veterans Services', 'goodbids' ),
+					__( 'Other', 'goodbids' ),
+				],
+			],
+			'finance_contact_name'  => [
+				'label'       => __( 'Finance Contact Legal Name', 'goodbids' ),
+				'type'        => 'text',
+				'default'     => '',
+				'placeholder' => '',
+				'required'    => true,
+			],
+			'finance_contact_email' => [
+				'label'       => __( 'Finance Contact Email Address', 'goodbids' ),
+				'type'        => 'email',
+				'default'     => '',
+				'placeholder' => 'email@domain.com',
+				'required'    => true,
+			],
+			'finance_contact_title' => [
+				'label'       => __( 'Finance Contact Job Title', 'goodbids' ),
+				'type'        => 'text',
+				'default'     => '',
+				'placeholder' => '',
+				'required'    => true,
 			],
 		];
 	}
--- client-mu-plugins/goodbids/views/admin/fields/select.php
+++ client-mu-plugins/goodbids/views/admin/fields/select.php
@@ -2,14 +2,14 @@
 /**
  * Admin Field: Select
  *
- * @global bool   $required
- * @global array  $field
- * @global string $prefix
- * @global string $key
- * @global string $placeholder
- * @global string $field_id
- * @global mixed  $value
- * @global bool   $wrap
+ * @var bool   $required
+ * @var array  $field
+ * @var string $prefix
+ * @var string $key
+ * @var string $placeholder
+ * @var string $field_id
+ * @var mixed  $value
+ * @var bool   $wrap
  *
  * @since 1.0.0
  * @package GoodBids
@@ -17,13 +17,13 @@
 
 ?>
 <?php if ( $wrap ) : ?>
-	<tr class="form-field<?php echo $required ? ' form-required' : ''; ?>">
+	<tr class="<?php echo $required ? ' form-required' : ''; ?>">
 		<th scope="row">
 			<label for="<?php echo esc_attr( $field_id ); ?>">
 				<?php
 				echo esc_html( $field['label'] );
 				if ( $required ) :
-					echo ' ' . wp_required_field_indicator(); // phpcs:ignore
+					echo '&nbsp;' . wp_required_field_indicator(); // phpcs:ignore
 				endif;
 				?>
 			</label>
@@ -47,12 +47,15 @@
 				<option value="" <?php selected( $value, '' ); ?>><?php esc_html_e( 'Select One', 'goodbids' ); ?></option>
 			<?php endif; ?>
 
-			<?php foreach ( $field['options'] as $option ) : ?>
+			<?php foreach ( $field['options'] as $index => $option ) :
+				$opt_label = is_string( $option ) ? $option : $option['label'];
+				$opt_value = ! is_numeric( $index ) ? $index : ( is_string( $option ) ? $option : $option['value'] );
+				?>
 				<option
-					value="<?php echo esc_attr( $option['value'] ); ?>"
-					<?php selected( $value, $option['value'] ); ?>
+					value="<?php echo esc_attr( $opt_value ); ?>"
+					<?php selected( $value, $opt_value ); ?>
 				>
-					<?php echo esc_html( $option['label'] ); ?>
+					<?php echo esc_html( $opt_label ); ?>
 				</option>
 			<?php endforeach; ?>
 		</select>
