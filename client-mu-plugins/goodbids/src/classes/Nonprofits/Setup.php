a478e7d42998e79bb547179f8d4d7d3a4128e095
Brian DiChiara <brian.dichiara@viget.com>
2024-03-01 12:20:56 -0600

    [#469] Some Locking down before Setup

--- client-mu-plugins/goodbids/src/classes/Nonprofits/Setup.php
+++ client-mu-plugins/goodbids/src/classes/Nonprofits/Setup.php
@@ -9,6 +9,7 @@
 namespace GoodBids\Nonprofits;
 
 use GoodBids\Network\Nonprofit;
+use GoodBids\Auctions\Wizard;
 
 /**
  * Setup Class
@@ -27,7 +28,14 @@ class Setup {
 	 * @since 1.0.0
 	 */
 	public function __construct() {
+		if ( is_main_site() || is_network_admin() ) {
+			return;
+		}
+
 		$this->nonprofit = new Nonprofit( get_current_blog_id() );
+
+		// Redirect to Setup page if not set up.
+		$this->force_setup();
 
 		// Remove any admin notices for this page.
 		$this->disable_admin_notices();
@@ -39,6 +47,69 @@ class Setup {
 		$this->enqueue_scripts();
 	}
 
+	/**
+	 * Require Setup
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	private function force_setup(): void {
+		add_action(
+			'current_screen',
+			function () {
+				if ( $this->setup_completed() || $this->is_setup_page() || is_super_admin() ) {
+					return;
+				}
+
+				$screen      = get_current_screen();
+				$setup_pages = [
+					'options-general',
+					'site-editor',
+					'edit-page',
+					'user',
+					'edit-comments',
+					'woocommerce_page_wc-admin',
+					'woocommerce_page_wc-settings',
+					'toplevel_page_jetpack',
+					'jetpack_page_akismet-key-config',
+					'toplevel_page_accessibility_checker',
+					Wizard::BASE_URL . goodbids()->invoices->get_post_type() . '&page=' . Wizard::PAGE_SLUG,
+					'edit-' . goodbids()->auctions->get_post_type(),
+					'edit-' . goodbids()->invoices->get_post_type(),
+				];
+
+				if ( in_array( $screen->id, $setup_pages, true ) ) {
+					return;
+				}
+
+				wp_safe_redirect( admin_url( 'admin.php?page=' . self::PAGE_SLUG ) );
+				exit;
+			}
+		);
+
+		add_action(
+			'admin_menu',
+			function () {
+				if ( $this->setup_completed() || is_super_admin() ) {
+					return;
+				}
+
+				global $menu;
+
+				foreach ( $menu as &$item ) {
+					if ( self::PAGE_SLUG !== $item[2] ) {
+						if ( isset( $item[4] ) ) {
+							$item[4] .= ' hidden';
+						} else {
+							$item[4] = 'hidden';
+						}
+					}
+				}
+			},
+			99999
+		);
+	}
 
 	/**
 	 * Add the menu page for the setup dashboard
@@ -48,6 +119,11 @@ class Setup {
 	 * @return void
 	 */
 	private function add_menu_dashboard_page(): void {
+		// Disable for the main site.n
+		if ( is_main_site() || $this->setup_completed() ) {
+			return;
+		}
+
 		add_action(
 			'admin_menu',
 			function () {
@@ -89,7 +165,7 @@ class Setup {
 			return false;
 		}
 
-		$page      = sanitize_text_field( wp_unslash( $_GET['page'] ) ); // phpcs:ignore
+		$page = sanitize_text_field( wp_unslash( $_GET['page'] ) ); // phpcs:ignore
 
 		if ( self::PAGE_SLUG !== $page ) {
 			return false;
@@ -159,31 +235,31 @@ class Setup {
 	 */
 	private function get_js_vars(): array {
 		return [
-			'appID'   => self::PAGE_SLUG,
-			'ajaxUrl' => admin_url( 'admin-ajax.php' ),
-			'optionsGeneralURL' => admin_url( 'options-general.php' ),
-			'createWooCommerceURL' => admin_url( 'admin.php?page=wc-admin&path=/setup-wizard&step=skip-guided-setup' ),
-			'setUpPaymentURL' => admin_url( 'admin.php?page=wc-settings&tab=checkout&section=stripe' ),
-			'configureShippingURL' => admin_url( 'admin.php?page=wc-settings&tab=shipping' ),
-			'jetpackURL' => admin_url( 'admin.php?page=jetpack#/dashboard' ),
-			'akismetURL' => admin_url( 'admin.php?page=akismet-key-config' ),
-			'woocommerceSettingsURL' => admin_url( 'admin.php?page=wc-settings&tab=general' ),
-			'styleURL' => admin_url( 'site-editor.php?path=/wp_global_styles' ),
-			'updateLogoURL' => admin_url( 'site-editor.php?postType=wp_template_part&postId=goodbids-nonprofit//header&categoryId=header&categoryType=wp_template_part' ),
-			'customizeHomepageURL' => admin_url( 'site-editor.php?postType=wp_template_part&postId=goodbids-nonprofit//header&categoryId=header&categoryType=wp_template_part' ),
-			'pagesURL' => admin_url( 'edit.php?post_type=page' ),
-			'patternsURL' => admin_url( 'site-editor.php?path=/patterns' ),
-			'auctionWizardURL' => admin_url( 'edit.php?post_type=gb-auction&page=gb-auction-wizard' ),
-			'addUsersURL' => admin_url( 'user-new.php' ),
+			'appID'                   => self::PAGE_SLUG,
+			'ajaxUrl'                 => admin_url( 'admin-ajax.php' ),
+			'optionsGeneralURL'       => admin_url( 'options-general.php' ),
+			'createWooCommerceURL'    => admin_url( 'admin.php?page=wc-admin&path=/setup-wizard&step=skip-guided-setup' ),
+			'setUpPaymentURL'         => admin_url( 'admin.php?page=wc-settings&tab=checkout&section=stripe' ),
+			'configureShippingURL'    => admin_url( 'admin.php?page=wc-settings&tab=shipping' ),
+			'jetpackURL'              => admin_url( 'admin.php?page=jetpack#/dashboard' ),
+			'akismetURL'              => admin_url( 'admin.php?page=akismet-key-config' ),
+			'woocommerceSettingsURL'  => admin_url( 'admin.php?page=wc-settings&tab=general' ),
+			'styleURL'                => admin_url( 'site-editor.php?path=/wp_global_styles' ),
+			'updateLogoURL'           => admin_url( 'site-editor.php?postType=wp_template_part&postId=goodbids-nonprofit//header&categoryId=header&categoryType=wp_template_part' ),
+			'customizeHomepageURL'    => admin_url( 'site-editor.php?postType=wp_template_part&postId=goodbids-nonprofit//header&categoryId=header&categoryType=wp_template_part' ),
+			'pagesURL'                => admin_url( 'edit.php?post_type=page' ),
+			'patternsURL'             => admin_url( 'site-editor.php?path=/patterns' ),
+			'auctionWizardURL'        => admin_url( Wizard::BASE_URL . goodbids()->invoices->get_post_type() . '&page=' . Wizard::PAGE_SLUG  ),
+			'addUsersURL'             => admin_url( 'user-new.php' ),
 			'accessibilityCheckerURL' => admin_url( 'admin.php?page=accessibility_checker' ),
-			'homeURL' => home_url(),
-			'auctionsURL' => admin_url( 'edit.php?post_type=gb-auction' ),
-			'orderMetricsURL' => admin_url( 'admin.php?page=wc-admin&path=/analytics/categories' ),
-			'revenueMetricsURL' => admin_url( 'admin.php?page=wc-admin&path=/analytics/revenue&chart=net_revenue&orderby=net_revenue' ),
-			'invoicesURL' => admin_url( 'edit.php?post_type=gb-invoice' ),
-			'commentsURL' => admin_url( 'edit-comments.php' ),
-			'siteStatus' => $this->nonprofit->get_status(),
-			'siteStatusOptions' => $this->nonprofit->get_site_status_options()
+			'homeURL'                 => home_url(),
+			'auctionsURL'             => admin_url( 'edit.php?post_type=' . goodbids()->invoices->get_post_type() ),
+			'orderMetricsURL'         => admin_url( 'admin.php?page=wc-admin&path=/analytics/categories' ),
+			'revenueMetricsURL'       => admin_url( 'admin.php?page=wc-admin&path=/analytics/revenue&chart=net_revenue&orderby=net_revenue' ),
+			'invoicesURL'             => admin_url( 'edit.php?post_type=' . goodbids()->invoices->get_post_type() ),
+			'commentsURL'             => admin_url( 'edit-comments.php' ),
+			'siteStatus' 			  => $this->nonprofit->get_status(),
+			'siteStatusOptions'       => $this->nonprofit->get_site_status_options()
 		];
 	}
 
@@ -205,5 +281,16 @@ class Setup {
 				remove_all_actions( 'admin_notices' );
 			}
 		);
+	}
+
+	/**
+	 * Check if the setup is completed.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return bool
+	 */
+	private function setup_completed(): bool {
+		return boolval( get_option( 'goodbids_nonprofit_setup_completed' ) );
 	}
 }
