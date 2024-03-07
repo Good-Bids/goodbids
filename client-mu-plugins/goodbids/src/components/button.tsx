4bd857adc7ab2420eafd40fc63a2a97df35f0eba
Nick <nick.telsan@viget.com>
2024-03-07 16:50:50 -0500

    [#] Hover AND focus (#635)
    - Fixes hover and focus states

--- client-mu-plugins/goodbids/src/components/button.tsx
+++ client-mu-plugins/goodbids/src/components/button.tsx
@@ -8,7 +8,7 @@ export function Button({ variant = 'outline', ...rest }: ButtonProps) {
 	const classes = clsx(
 		'w-full cursor-pointer rounded-full px-6 py-3 text-gb-md outline-none transition-all focus:ring-2 focus:ring-offset-2 active:animate-pulse disabled:cursor-not-allowed',
 		{
-			'border-none bg-gb-green-700 text-white hover:bg-gb-green-100 hover:text-gb-green-900 focus:ring-gb-green-700   disabled:bg-gb-gray-500 disabled:text-white':
+			'border-none bg-gb-green-700 text-white hover:bg-gb-green-100 hover:text-gb-green-900 focus:ring-gb-green-700 hover:focus:text-gb-green-900 disabled:bg-gb-gray-500 disabled:text-white':
 				variant === 'solid',
 			'border-2 border-solid border-gb-green-700 bg-transparent text-gb-green-700 hover:bg-gb-green-100 hover:text-gb-green-900 focus:ring-gb-green-700 disabled:bg-gb-gray-500':
 				variant === 'outline',
