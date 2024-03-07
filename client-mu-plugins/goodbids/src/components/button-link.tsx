06e47ce0080c6aac990d8e930cab85d27cf241ad
Nick Telsan <nick.telsan@viget.com>
2024-03-07 14:49:33 -0500

    [#] Button Link Focus
    - Fixes text color on focused button links

--- client-mu-plugins/goodbids/src/components/button-link.tsx
+++ client-mu-plugins/goodbids/src/components/button-link.tsx
@@ -8,11 +8,11 @@ export function ButtonLink({ variant = 'outline', ...rest }: ButtonProps) {
 	const classes = clsx(
 		'box-border block w-full cursor-pointer rounded-full px-6 py-3 text-center text-gb-md no-underline outline-none transition-all focus:ring-2 focus:ring-offset-2 active:animate-pulse disabled:cursor-not-allowed',
 		{
-			'border-none bg-gb-green-700 text-white hover:bg-gb-green-100 hover:text-gb-green-900 focus:ring-gb-green-700 disabled:text-white':
+			'border-none bg-gb-green-700 text-white hover:bg-gb-green-100 hover:text-gb-green-900 focus:text-white focus:ring-gb-green-700 disabled:text-white':
 				variant === 'solid',
-			'border-2 border-solid border-gb-green-700 bg-transparent text-gb-green-700 hover:bg-gb-green-100 hover:text-gb-green-900 focus:ring-gb-green-700':
+			'border-2 border-solid border-gb-green-700 bg-transparent text-gb-green-700 hover:bg-gb-green-100 hover:text-gb-green-900 focus:text-gb-green-700 focus:ring-gb-green-700':
 				variant === 'outline',
-			'border-none bg-transparent text-gb-green-700 hover:bg-gb-green-100 hover:underline focus:underline focus:ring-gb-green-700':
+			'border-none bg-transparent text-gb-green-700 hover:bg-gb-green-100 hover:underline focus:text-gb-green-700 focus:underline focus:ring-gb-green-700':
 				variant === 'ghost',
 		},
 	);
