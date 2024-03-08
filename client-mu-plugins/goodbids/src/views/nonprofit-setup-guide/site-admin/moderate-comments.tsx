fe3531d434e126eec43d879c70a3b6a4581e19f1
Claire Atwell <claire.eisinger@viget.com>
2024-03-08 16:02:01 -0500

    [#659] import footnote for moderate comments

--- client-mu-plugins/goodbids/src/views/nonprofit-setup-guide/site-admin/moderate-comments.tsx
+++ client-mu-plugins/goodbids/src/views/nonprofit-setup-guide/site-admin/moderate-comments.tsx
@@ -1,6 +1,7 @@
 import { __ } from '@wordpress/i18n';
 import { MultiStepHeading } from '../components/multi-step-heading';
 import { ButtonLink } from '../../../components/button-link';
+import { Footnote } from '../components/footnote';
 
 export function ModerateComments() {
 	return (
