3de688ba22502ca9bdcf46a2d79c3d72c275558e
Claire Atwell <claire.eisinger@viget.com>
2024-03-08 15:55:38 -0500

    [#659] Adjust Upload Logo content

--- client-mu-plugins/goodbids/src/views/nonprofit-setup-guide/design-and-content/upload-logo.tsx
+++ client-mu-plugins/goodbids/src/views/nonprofit-setup-guide/design-and-content/upload-logo.tsx
@@ -5,21 +5,11 @@ import { MultiStepHeading } from '../components/multi-step-heading';
 export function UploadLogo() {
 	return (
 		<MultiStepHeading
-			title={__('Upload Logo', 'goodbids')}
+			title={__('Upload Site Logo', 'goodbids')}
 			content={
 				<>
 					{__(
-						'Upload a logo with square dimensions no less than 48x48px by navigating to',
-						'goodbids',
-					)}{' '}
-					<i>
-						{__(
-							'Design > Patterns > Template Parts > Header',
-							'goodbids',
-						)}
-					</i>
-					{__(
-						'in the Appearance Editor. Once there, click on the Header to start editing the template. Then click the Logo area to access the component. Select the "Add a Site Logo" upload button to the left of the site title to open the Media Library. Once you have successfully added your logo, click Save to update the template.',
+						'Create a JPG or PNG logo file with square dimensions no less than 48px48px. Click the button below, then click on the Header to edit the template. Click the logo area, then use the upload icon to add your image. Click Save to update the template.',
 						'goodbids',
 					)}
 				</>
@@ -31,9 +21,15 @@ export function UploadLogo() {
 					variant="solid"
 					href={gbNonprofitSetupGuide.uploadLogoURL}
 				>
-					{__('Modify Site Header', 'goodbids')}
+					{__('Add Logo to Site Header', 'goodbids')}
 				</ButtonLink>
 			</div>
+
+			<Footnote>
+				{__('This button takes you to the')}{' '}
+				<i>{__('Design > Patterns > Template Parts > Header', 'goodbids')}</i> {__('section of the Apperance Editor.')}
+			</Footnote>
+			
 		</MultiStepHeading>
 	);
 }
