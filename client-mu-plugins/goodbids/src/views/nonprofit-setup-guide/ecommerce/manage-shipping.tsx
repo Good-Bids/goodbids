1508fe2097ba2789d0b68ea7476f3a41f73d4807
Claire Atwell <claire.eisinger@viget.com>
2024-03-08 15:42:19 -0500

    [#659] Adjust content for Manage Shipping

--- client-mu-plugins/goodbids/src/views/nonprofit-setup-guide/ecommerce/manage-shipping.tsx
+++ client-mu-plugins/goodbids/src/views/nonprofit-setup-guide/ecommerce/manage-shipping.tsx
@@ -9,7 +9,7 @@ export function ManageShipping() {
 			content={
 				<>
 					{__(
-						'To support auctions for physical products, you will need to configure shipping for your WooCommerce store. Create shipping zones for the region(s) you’d like to ship to and shipping method(s) offered. Free shipping, flat rate, and local pickup shipping methods are available. You can also use shipping classes to customize the shipping rates for different groups of products, such as heavy items that require higher postage fees.',
+						'You will need to configure shipping to support auctions for physical products. Create shipping zones for the region(s) you’d like to ship to and shipping method(s) offered. Free shipping, flat rate, and local pickup shipping methods are available. Add shipping classes if you need to customize the shipping rates for different groups of products, such as heavy items that require higher postage fees.',
 						'goodbids',
 					)}{' '}
 					<b>
@@ -30,6 +30,11 @@ export function ManageShipping() {
 					{__('Add Shipping Zones', 'goodbids')}
 				</ButtonLink>
 			</div>
+
+			<Footnote>
+				{__('This button takes you to the')}{' '}
+				<i>{__('WooCommerce > Settings > Shipping', 'goodbids')}</i> {__('page.')}
+			</Footnote>
 		</MultiStepHeading>
 	);
 }
