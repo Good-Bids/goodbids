029c0cfc395bdf789fac1c7e80e039910721bde2
Nick Telsan <nick.telsan@viget.com>
2024-03-12 16:11:58 -0400

    Split live text into 2 paragrahs

--- client-mu-plugins/goodbids/src/views/nonprofit-setup-guide/site-status/live.tsx
+++ client-mu-plugins/goodbids/src/views/nonprofit-setup-guide/site-status/live.tsx
@@ -5,7 +5,6 @@ export function Live() {
 	return (
 		<>
 			<H1>{__('Live', 'goodbids')}</H1>
-
 			<P>
 				{__('Now that your site is live, review the', 'goodbids')}{' '}
 				<a href={gbNonprofitSetupGuide.accessibilityCheckerURL}>
@@ -15,6 +14,9 @@ export function Live() {
 					'and resolve any issues flagged on your pages.',
 					'goodbids',
 				)}
+			</P>
+
+			<P>
 				{__(
 					'Please refer to the Nonprofit Site Guide for details on managing your site, planning auctions, and getting the most out of the GOODBIDS network!',
 					'goodbids',
