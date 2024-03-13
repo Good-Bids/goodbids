0e0f7ff698b00efae9f1c12c9985d5b888b294a4
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-03-13 15:19:34 -0600

    [#720] updating footer

--- themes/goodbids-main/footer-wp-activate.php
+++ themes/goodbids-main/footer-wp-activate.php
@@ -2,8 +2,21 @@
 
 	</div>
 	<hr/>
-	<p>
-		<?php echo $post ? esc_html( $post->post_title ) . ' | ' : ''; ?><?php echo esc_html( get_option( 'blogname' ) ); ?>
-	</p>
+	<div class="activate-footer">
+		<p>
+			<?php
+			if ( get_option( 'blogdescription' ) ) {
+				$blogdescription = '| ' . get_option( 'blogdescription' );
+			}
+
+			printf(
+				/* translators: %1$s: Site name, %2$s: Site description. */
+				__( '%1$s %2$s', 'goodbids' ),
+				esc_html( get_option( 'blogname' ) ),
+				esc_html( $blogdescription )
+			);
+			?>
+		</p>
+	</div>
 </body>
 </html>
--- themes/goodbids-main/header-wp-activate.php
+++ themes/goodbids-main/header-wp-activate.php
@@ -16,21 +16,41 @@ global $post;
 	</title>
 </head>
 <body>
-	<div id="activate-header">
-		<h1 class="text-lg header-title">Thanks for Joining <?php echo esc_html( get_option( 'blogname' ) ); ?> on GOODBIDS!</h1>
-		<h2 class="text-md">What's next?</h2>
+	<div class="activate-header">
+		<?php if ( get_custom_logo( get_main_site_id() ) ) : ?>
+			<figure>
+				<?php echo wp_kses_post( get_custom_logo( get_main_site_id() ) ); ?>
+			</figure>
+		<?php endif; ?>
+		<h1 class="text-lg header-title">
+		<?php
+			printf(
+				/* translators: %s: site name */
+				__( 'Thanks for Joining %s on GOODBIDS!', 'goodbids' ),
+				esc_html( get_option( 'blogname' ) )
+			)
+			?>
+		</h1>
+		<h2 class="text-md"><?php esc_html_e( 'What\'s next?', 'goodbids' ); ?></h2>
 		<ol>
-			<li>Review the Nonprofit <?php echo wp_kses_post( goodbids()->sites->get_terms_conditions_link() ); ?></li>
 			<li>
-				Log in to GOODBIDS with your username and temporary password shown below.</br>
-				<small>Note: This information was also sent to you via email.</small>
-				<p><a class="wp-block-button__link wp-element-button" href="<?php echo esc_url( wp_login_url() ); ?>">Login</a></p>
+				<?php
+				printf(
+					__( 'Review the Nonprofit %s.', 'goodbids' ),
+					wp_kses_post( goodbids()->sites->get_terms_conditions_link() )
+				);
+				?>
 			</li>
 			<li>
-				Enable Two-Factor authentication and set a new password for your account.</br>
-				<small>Note: Two-Factor authentication is required.</small>
+				<?php esc_html_e( 'Log in to GOODBIDS with your username and temporary password shown below.', 'goodbids' ); ?></br>
+				<small><?php esc_html_e( 'Note: This information was also sent to you via email.', 'goodbids' ); ?></small>
+				<p><a class="wp-block-button__link wp-element-button" href="<?php echo esc_url( wp_login_url() ); ?>"><?php esc_html_e( 'Login', 'goodbids' ); ?></a></p>
+			</li>
+			<li>
+				<?php esc_html_e( 'Enable Two-Factor authentication and set a new password for your account.', 'goodbids' ); ?></br>
+				<small><?php esc_html_e( 'Note: Two-Factor authentication is required.', 'goodbids' ); ?></small>
 			</li>
 		</ol>
 	</div>
 	<hr/>
-	<div id="activate-content">
+	<div class="activate-content">
