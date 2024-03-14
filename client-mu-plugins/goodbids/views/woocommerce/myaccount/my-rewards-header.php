b31bebbe49f9d87e4406302002943f5991b457a8
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-03-14 12:47:39 -0600

    [#728] Removing the font thin class (#755)

--- client-mu-plugins/goodbids/views/woocommerce/myaccount/my-rewards-header.php
+++ client-mu-plugins/goodbids/views/woocommerce/myaccount/my-rewards-header.php
@@ -13,17 +13,17 @@ $unclaimed = $earned - $claimed;
 <div class="mb-6 goodbids-rewards-header">
 	<ul class="flex flex-wrap gap-6 p-0 m-0 list-none md:grid md:grid-cols-3 goodbids-order-metrics">
 		<li class="w-full p-4 rounded-sm sm:w-auto bg-contrast-5">
-			<p class="m-0 font-thin uppercase has-x-small-font-size"><?php esc_html_e( 'Earned', 'goodbids' ); ?></p>
+			<p class="m-0 uppercase has-x-small-font-size"><?php esc_html_e( 'Earned', 'goodbids' ); ?></p>
 			<p class="mt-2 mb-0 text-lg font-bold"><?php echo esc_html( $earned ); ?></p>
 		</li>
 
 		<li class="w-full p-4 rounded-sm sm:w-auto bg-contrast-5">
-			<p class="m-0 font-thin uppercase has-x-small-font-size"><?php esc_html_e( 'Claimed', 'goodbids' ); ?></p>
+			<p class="m-0 uppercase has-x-small-font-size"><?php esc_html_e( 'Claimed', 'goodbids' ); ?></p>
 			<p class="mt-2 mb-0 text-lg font-bold"><?php echo esc_html( $claimed ); ?></p>
 		</li>
 
 		<li class="w-full p-4 rounded-sm sm:w-auto bg-contrast-5">
-			<p class="m-0 font-thin uppercase has-x-small-font-size"><?php esc_html_e( 'Unclaimed', 'goodbids' ); ?></p>
+			<p class="m-0 uppercase has-x-small-font-size"><?php esc_html_e( 'Unclaimed', 'goodbids' ); ?></p>
 			<p class="mt-2 mb-0 text-lg font-bold"><?php echo esc_html( $unclaimed ); ?></p>
 		</li>
 	</ul>
