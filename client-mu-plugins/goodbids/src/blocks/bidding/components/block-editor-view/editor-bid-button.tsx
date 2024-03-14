ea243d5edea3e9d42022a2d56d65ff12166f6723
Nick Telsan <nick.telsan@viget.com>
2024-03-14 14:33:00 -0400

    [#744] Editor Bidding Block text
    - Adjusts editor bidding block text

--- client-mu-plugins/goodbids/src/blocks/bidding/components/block-editor-view/editor-bid-button.tsx
+++ client-mu-plugins/goodbids/src/blocks/bidding/components/block-editor-view/editor-bid-button.tsx
@@ -3,7 +3,8 @@ import { __ } from '@wordpress/i18n';
 export function EditorBidButton() {
 	return (
 		<span className="btn-fill text-center">
-			{__('GOODBID', 'goodbids')}
+			{__('GOODBID', 'goodbids')} <i>{__('$bid_amount', 'goodbids')}</i>{' '}
+			{__('now', 'goodbids')}
 		</span>
 	);
 }
