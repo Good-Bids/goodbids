b45e1f182abfd56f9cc8d355ae9453e812a45d59
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-03-14 11:48:08 -0600

    [#728] Removing the font thin class

--- client-mu-plugins/goodbids/views/woocommerce/myaccount/my-auctions-header.php
+++ client-mu-plugins/goodbids/views/woocommerce/myaccount/my-auctions-header.php
@@ -11,17 +11,17 @@
 
 	<ul class="flex flex-wrap gap-6 p-0 m-0 list-none md:grid md:grid-cols-3 goodbids-order-metrics">
 		<li class="w-full p-4 rounded-sm sm:w-auto bg-contrast-5">
-			<p class="m-0 font-thin uppercase has-x-small-font-size"><?php esc_html_e( 'Total Auctions', 'goodbids' ); ?></p>
+			<p class="m-0 uppercase has-x-small-font-size"><?php esc_html_e( 'Total Auctions', 'goodbids' ); ?></p>
 			<p class="mt-2 mb-0 text-lg font-bold"><?php echo esc_html( count( goodbids()->sites->get_user_participating_auctions() ) ); ?></p>
 		</li>
 
 		<li class="w-full p-4 rounded-sm sm:w-auto bg-contrast-5">
-			<p class="m-0 font-thin uppercase has-x-small-font-size"><?php esc_html_e( 'Live Auctions', 'goodbids' ); ?></p>
+			<p class="m-0 uppercase has-x-small-font-size"><?php esc_html_e( 'Live Auctions', 'goodbids' ); ?></p>
 			<p class="mt-2 mb-0 text-lg font-bold"><?php echo esc_html( count( goodbids()->sites->get_user_live_participating_auctions() ) ); ?></p>
 		</li>
 
 		<li class="w-full p-4 rounded-sm sm:w-auto bg-contrast-5">
-			<p class="m-0 font-thin uppercase has-x-small-font-size"><?php esc_html_e( 'Auctions Won', 'goodbids' ); ?></p>
+			<p class="m-0 uppercase has-x-small-font-size"><?php esc_html_e( 'Auctions Won', 'goodbids' ); ?></p>
 			<p class="mt-2 mb-0 text-lg font-bold"><?php echo esc_html( count( goodbids()->sites->get_user_auctions_won() ) ); ?></p>
 		</li>
 	</ul>
