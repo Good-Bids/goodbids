d5b707c33ba814718ea19f7fde91184a64fbd3b9
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-03-15 15:04:41 -0600

    [#684] updating textarea

--- client-mu-plugins/goodbids/views/forms/textarea.php
+++ client-mu-plugins/goodbids/views/forms/textarea.php
@@ -16,14 +16,14 @@
  * @package GoodBids
  */
 
-$class = 'flex-1 block p-4 text-base bg-transparent border rounded-sm resize-y border-base focus:outline-contrast-3';
+$class = 'flex-1 block p-4 text-base bg-transparent border rounded-sm resize-y border-base focus:outline-contrast-3 placeholder:text-base';
 
 if ( ! empty( $field['class'] ) ) {
 	$class .= ' ' . $field['class'];
 }
 ?>
 <?php if ( $wrap ) : ?>
-	<div class="mb-6">
+	<div class="flex flex-col mb-6">
 		<label for="<?php echo esc_attr( $field_id ); ?>" class="block mb-2 text-sm font-medium text-base<?php echo $required ? ' form-required' : ''; ?>">
 			<?php
 			echo esc_html( $field['label'] );
