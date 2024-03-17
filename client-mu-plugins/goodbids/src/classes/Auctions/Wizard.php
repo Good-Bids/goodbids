460f7eb4d1148f7d2d42171511bf7b0409278290
Brian DiChiara <brian.dichiara@viget.com>
2024-03-17 16:18:49 -0500

    [N/A] Fix Merge Bug

--- client-mu-plugins/goodbids/src/classes/Auctions/Wizard.php
+++ client-mu-plugins/goodbids/src/classes/Auctions/Wizard.php
@@ -329,7 +329,7 @@ class Wizard {
 			],
 			'auctionIdParam'   => self::AUCTION_ID_PARAM,
 			'rewardIdParam'    => self::REWARD_ID_PARAM,
-			'useFreeBidParam'  => Bids::USE_FREE_BID_PARAM,
+			'useFreeBidParam'  => FreeBids::USE_FREE_BID_PARAM,
 
 			// Flags.
 			'metricsEnabled' => false,
