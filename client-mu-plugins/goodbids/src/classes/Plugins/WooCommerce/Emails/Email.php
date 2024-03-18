9fab46d99bf5264fc58affc38918e8eda6b89149
Brian DiChiara <brian.dichiara@viget.com>
2024-03-18 14:00:48 -0500

    [N/A] Add did_init flag for emails (test)

--- client-mu-plugins/goodbids/src/classes/Plugins/WooCommerce/Emails/Email.php
+++ client-mu-plugins/goodbids/src/classes/Plugins/WooCommerce/Emails/Email.php
@@ -84,6 +84,15 @@ class Email extends WC_Email {
 	public $recipient = '';
 
 	/**
+	 * Check if this has been initialized yet.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var bool
+	 */
+	protected $did_init = false;
+
+	/**
 	 * Set default vars and placeholders
 	 *
 	 * @since 1.0.0
