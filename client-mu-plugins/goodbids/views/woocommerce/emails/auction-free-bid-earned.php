b13a581fc482b7695bea2739f71a45fd140a4cf2
Claire Atwell <claire.eisinger@viget.com>
2024-03-26 12:42:25 -0400

    Update auction-free-bid-earned.php

--- client-mu-plugins/goodbids/views/woocommerce/emails/auction-free-bid-earned.php
+++ client-mu-plugins/goodbids/views/woocommerce/emails/auction-free-bid-earned.php
@@ -38,10 +38,14 @@ do_action( 'woocommerce_email_header', $email_heading );
 <p>
 	<?php
 	printf(
-		/* translators: %1$s: Free Bids */
-		esc_html__( 'You have %1$s Free Bids that can be used in any eligible auction. You can earn more by placing one of the first five paid bids in any auction, or by', 'goodbids' ),
-		'TODO {user.free_bid_count}',
-		'TODO <a href="#">referring to friend</a>',
+		/* translators: %1$s: Number of Free Bids, %2$s Refer a Friend link */
+		esc_html__( 'You have %1$s Free Bids that can be used in any eligible auction. You can earn more by placing one of the first five paid bids in any auction, or by %2$s', 'goodbids' ),
+		'{user.free_bid_count}',
+		sprintf(
+			'<a href="%s">%s</a>.',
+			'{user.referral_link}',
+			esc_html__( 'referring a friend', 'goodbids' )
+		)
 	);
 	?>
 </p>
