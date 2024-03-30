2f22d201a2b4c9c185ea0515a7dde43120248042
Jasper Croome <jaspercroome@protonmail.com>
2024-03-30 09:37:59 -0700

    adding redirect logic per https://docs.wpvip.com/redirects/domain-redirects-in-vip-config-php/

--- vip-config/vip-config.php
+++ vip-config/vip-config.php
@@ -90,3 +90,32 @@ if ( ! defined( 'WP_DEBUG' ) ) {
 		}
 	}
 }
+
+
+/**
+ * Redirect queries from goodbids.org to www.goodbids.org
+ * @see https://docs.wpvip.com/redirects/domain-redirects-in-vip-config-php/
+ */
+if ( isset( $_SERVER['HTTP_HOST'] ) && isset( $_SERVER['REQUEST_URI'] ) ) {
+	$http_host   = $_SERVER['HTTP_HOST']; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+	$request_uri = $_SERVER['REQUEST_URI']; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+
+	$redirect_to_domain = 'www.goodbids.org';
+	$redirect_domains   = array(
+		'goodbids.org',
+	);
+
+	/**
+	 * Safety checks for redirection:
+	 * 1. Don't redirect for '/cache-healthcheck?' or monitoring will break
+	 * 2. Don't redirect in WP CLI context
+	 */
+	if (
+			'/cache-healthcheck?' !== $request_uri && // Do not redirect VIP's monitoring.
+			! ( defined( 'WP_CLI' ) && WP_CLI ) && // Do not redirect WP-CLI commands.
+			$redirect_to_domain !== $http_host && in_array( $http_host, $redirect_domains, true )
+		) {
+		header( 'Location: https://' . $redirect_to_domain . $request_uri, true, 301 );
+		exit;
+	}
+}
