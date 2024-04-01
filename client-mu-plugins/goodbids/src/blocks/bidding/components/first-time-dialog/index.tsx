ec423169dbef39559a25f8d93a8ba8d282c33668
Jasper Croome <jaspercroome@protonmail.com>
2024-04-01 09:06:49 -0700

    feat: add first-time dialog

--- client-mu-plugins/goodbids/src/blocks/bidding/components/first-time-dialog/index.tsx
+++ client-mu-plugins/goodbids/src/blocks/bidding/components/first-time-dialog/index.tsx
@@ -6,8 +6,10 @@ import { WarningIconFilled } from '../icons/warning-icon-filled';
 export const FirstTimeDialog = () => {
 	const [hasSeenDialog, setHasSeenDialog] = useState(true);
 
+	const cookieString = 'gb-has-seen-first-time-dialog';
+
 	useEffect(() => {
-		const cookie = localStorage.getItem('gb-has-seen-first-time-dialog');
+		const cookie = localStorage.getItem(cookieString);
 		if (cookie !== null) {
 			const booledCookie = Boolean(cookie);
 			setHasSeenDialog(booledCookie);
@@ -16,42 +18,53 @@ export const FirstTimeDialog = () => {
 		}
 	}, []);
 
+	const handleSuccess = () => {
+		setHasSeenDialog(true);
+		localStorage.setItem(cookieString, 'true');
+	};
+
 	const dialogClasses = clsx(
-		'opacity-1 absolute overflow-hidden rounded-xl border-red-50 p-12 text-left shadow-xl sm:my-8 sm:w-[480px] sm:max-w-lg',
+		'opacity-1 absolute overflow-hidden rounded-xl border-red-50 p-12 text-left shadow-xl sm:my-8 sm:w-[384px] sm:max-w-lg',
 	);
 
 	return (
 		<AnimatePresence>
 			{!hasSeenDialog && (
 				<motion.dialog className={dialogClasses} open={!hasSeenDialog}>
-					<div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-						<div className="flex-col sm:flex sm:items-start">
-							<WarningIconFilled />
-							<div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-								<h3 className="font-bolder text-base leading-6 text-gray-900">
-									Every Bid is a Donation
-								</h3>
-								<div className="mt-2">
-									<p className="text-sm text-gray-500">
-										Welcome! This is your first bid, and we
-										want to remind you that all bids are
-										non-refundable donations. Please type
-										the word DONATION below to confirm that
-										you understand that bids go directly to
-										the charity and are non-refundable.
-									</p>
-								</div>
+					<div className="flex-col sm:flex sm:items-start">
+						<WarningIconFilled />
+						<div className="mt-3 text-center sm:mt-0 sm:text-left">
+							<h3 className="font-bolder text-base text-lg leading-6 text-gray-900">
+								Every Bid is a Donation
+							</h3>
+							<div className="mt-2">
+								<p className="text-sm text-gb-green-700">
+									Welcome! This is your first bid, and we want
+									to remind you that all bids are
+									non-refundable donations. Please type the
+									word DONATION below to confirm that you
+									understand that bids go directly to the
+									charity and are non-refundable.
+								</p>
 							</div>
 						</div>
 					</div>
-					<div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-						<form onSubmit={() => setHasSeenDialog(true)}>
-							<input pattern="DONATION" required />
-						</form>
-						<button className="bg-green-900 text-white">
-							I understand
-						</button>
-					</div>
+					<form
+						className="flex w-full flex-col gap-1"
+						onSubmit={handleSuccess}
+					>
+						<input
+							required
+							pattern="DONATION"
+							placeholder="DONATION"
+							className="rounded border border-solid border-transparent bg-gray-100 px-6 py-2 leading-normal no-underline focus:outline-dotted focus:outline-1 focus:outline-offset-2"
+						/>
+						<input
+							type="submit"
+							className="btn-fill w-full text-md"
+							value="I understand"
+						></input>
+					</form>
 				</motion.dialog>
 			)}
 		</AnimatePresence>
