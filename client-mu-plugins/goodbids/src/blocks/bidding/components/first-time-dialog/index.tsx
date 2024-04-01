5711e67a86f0e66b9dc4db8eb7ecab357a4b93a5
Jasper Croome <jaspercroome@protonmail.com>
2024-04-01 10:29:33 -0700

    fix: adjust location

--- client-mu-plugins/goodbids/src/blocks/bidding/components/first-time-dialog/index.tsx
+++ client-mu-plugins/goodbids/src/blocks/bidding/components/first-time-dialog/index.tsx
@@ -29,7 +29,7 @@ export const FirstTimeDialog = () => {
 
 	return (
 		!hasSeenDialog && (
-			<div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
+			<div className="fixed inset-0 left-0 top-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
 				<dialog className={dialogClasses} open={!hasSeenDialog}>
 					<div className="flex-col sm:flex sm:items-start">
 						<WarningIconFilled />
