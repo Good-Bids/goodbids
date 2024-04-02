a86a389c161ed29e5f8ad4d7df7f102be2d9f05b
jaspercroome <128058464+jaspercroome@users.noreply.github.com>
2024-04-01 19:50:30 -0700

    chore: disable production reroute for miniorange license key (#855)

--- vip-config/vip-config.php
+++ vip-config/vip-config.php
@@ -102,7 +102,7 @@ if ( isset( $_SERVER['HTTP_HOST'] ) && isset( $_SERVER['REQUEST_URI'] ) ) {
 
 	$redirect_to_domain = 'www.goodbids.org';
 	$redirect_domains   = array(
-		'production.goodbids.org',
+		// 'production.goodbids.org', TODO <-- add back in once miniorange key is switched over
 		'goodbids.org',
 	);
 
