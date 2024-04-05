ab1a0312a058de6c665bd44d95f44a9dedaa4d02
Claire Atwell <claire.eisinger@viget.com>
2024-04-05 14:12:29 -0400

    [n/a] Add helper text below Setup Guide header

--- client-mu-plugins/goodbids/src/views/nonprofit-setup-guide/driver.tsx
+++ client-mu-plugins/goodbids/src/views/nonprofit-setup-guide/driver.tsx
@@ -17,6 +17,7 @@ export function Driver() {
 			<Wrapper>
 				<div className="flex max-w-2xl flex-col items-start gap-4">
 					<H1>{__('Site Setup', 'goodbids')}</H1>
+					<P>Please note: The Setup Guide is an online instructional manual. The box titled “Site Status” (on the top right) is the only section that will update based on your progress through the guide. The Site Status will update to reflect the current status of your Nonprofit Site, and will either read “Pending” or “Live,” along with information relevant to that status level.</P>
 					{(gbNonprofitSetupGuide.isBDPAdmin ||
 						gbNonprofitSetupGuide.isAdmin) && (
 						<>
