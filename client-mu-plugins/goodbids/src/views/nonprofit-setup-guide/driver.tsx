3228302de2f51159d8e9e1369c49c49da69570b5
Nick Telsan <nick.telsan@viget.com>
2024-04-05 14:23:51 -0400

    Fixes formatting and imports

--- client-mu-plugins/goodbids/src/views/nonprofit-setup-guide/driver.tsx
+++ client-mu-plugins/goodbids/src/views/nonprofit-setup-guide/driver.tsx
@@ -1,5 +1,5 @@
 import { __ } from '@wordpress/i18n';
-import { H1 } from '../../components/typography';
+import { H1, P } from '../../components/typography';
 import { PlanAndMonitor } from './plan-and-monitor';
 import { Wrapper } from './wrapper';
 import { SiteStatus } from './site-status';
@@ -17,7 +17,12 @@ export function Driver() {
 			<Wrapper>
 				<div className="flex max-w-2xl flex-col items-start gap-4">
 					<H1>{__('Site Setup', 'goodbids')}</H1>
-					<P>{_('Please note: The Setup Guide is an online instructional manual. The box titled “Site Status” (on the top right) is the only section that will update based on your progress through the guide. The Site Status will update to reflect the current status of your Nonprofit Site, and will either read “Pending” or “Live,” along with information relevant to that status level.', 'goodbids')}</P>
+					<P>
+						{__(
+							'Please note: The Setup Guide is an online instructional manual. The box titled “Site Status” (on the top right) is the only section that will update based on your progress through the guide. The Site Status will update to reflect the current status of your Nonprofit Site, and will either read “Pending” or “Live,” along with information relevant to that status level.',
+							'goodbids',
+						)}
+					</P>
 					{(gbNonprofitSetupGuide.isBDPAdmin ||
 						gbNonprofitSetupGuide.isAdmin) && (
 						<>
